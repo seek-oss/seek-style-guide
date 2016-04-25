c106f9b195174c80c48cbedcf2f38d813b2f9613
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2016-04-26 08:38:03 +1000

    Co-locate typography story with theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,11 +2,11 @@ import { configure } from '@kadira/storybook';
 import '../theme/app.less';
 
 const components = require.context('../react', true, /\.story\.js$/);
-const other = require.context('../stories', true, /\.story\.js$/);
+const theme = require.context('../theme', true, /\.story\.js$/);
 
 function loadStories() {
   components.keys().forEach(components);
-  other.keys().forEach(other);
+  theme.keys().forEach(theme);
 }
 
 configure(loadStories, module);
