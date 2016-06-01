c3276c12a350efc651288e5f361c84d607738575
Misha Moroshko <michael.moroshko@gmail.com>
2016-06-02 08:47:13 +1000

    Update buttons text

--- app/components/Home/components/Hero/components/ButtonsPreview/ButtonsPreview.js
+++ app/components/Home/components/Hero/components/ButtonsPreview/ButtonsPreview.js
@@ -2,20 +2,21 @@ import styles from './ButtonsPreview.less';
 import buttonStyles from 'seek-style-guide/react/Button/Button.less';
 
 import React from 'react';
+import classnames from 'classnames';
 import { Button } from 'seek-style-guide/react';
 
 export default function ButtonsPreview() {
   return (
     <div className={styles.root}>
       <div className={styles.group}>
-        <Button colour="pink">SEEK</Button>
-        <Button colour="pink" className={buttonStyles.rootHover}>SEEK</Button>
-        <Button colour="pink" className={buttonStyles.rootActive}>SEEK</Button>
+        <Button colour="pink" className={styles.button}>Normal</Button>
+        <Button colour="pink" className={classnames(styles.button, buttonStyles.rootHover)}>Hover</Button>
+        <Button colour="pink" className={classnames(styles.button, buttonStyles.rootActive)}>Pressed</Button>
       </div>
       <div className={styles.group}>
-        <Button colour="blue">SEEK</Button>
-        <Button colour="blue" className={buttonStyles.rootHover}>SEEK</Button>
-        <Button colour="blue" className={buttonStyles.rootActive}>SEEK</Button>
+        <Button colour="blue" className={styles.button}>Normal</Button>
+        <Button colour="blue" className={classnames(styles.button, buttonStyles.rootHover)}>Hover</Button>
+        <Button colour="blue" className={classnames(styles.button, buttonStyles.rootActive)}>Pressed</Button>
       </div>
     </div>
   );
