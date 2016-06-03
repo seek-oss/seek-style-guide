7f7404ff34ff21b2dd5a22719d2f14e12f82d783
Michael Taranto <mtaranto@seek.com.au>
2016-06-03 12:28:30 +1000

    Add nav

--- app/components/App/components/Header/Header.js
+++ app/components/App/components/Header/Header.js
@@ -1,15 +1,25 @@
 import styles from './Header.less';
 
 import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
 import { Logo } from 'seek-style-guide/react';
 import GridContainer from 'GridContainer/GridContainer';
 
 export default function Header({ title }) {
   return (
     <GridContainer>
-      <div className={styles.logo}>
-        <Logo />
-        <p className={styles.title}>{title}</p>
+      <div className={styles.root}>
+        <div className={styles.logo}>
+          <Logo />
+          <p className={styles.title}>{title}</p>
+        </div>
+        <div>
+          <Link className={styles.link} activeClassName={styles.active} to="/" onlyActiveOnIndex={true}>Home</Link>
+          <Link className={styles.link} activeClassName={styles.active} to="typography">Typography</Link>
+          <Link className={styles.link} activeClassName={styles.active} to="buttons">Buttons</Link>
+          <Link className={styles.link} activeClassName={styles.active} to="textfields">Textfields</Link>
+          <Link className={styles.link} activeClassName={styles.active} to="icons">Icons</Link>
+        </div>
       </div>
     </GridContainer>
   );
