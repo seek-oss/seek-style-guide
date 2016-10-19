d5dcff5824733140cfe0f022ab7279f7ec08858d
Michael Taranto <mtaranto@seek.com.au>
2016-10-19 16:21:32 +1100

    Refactor to use renderInputComponent

--- react/fields/Autosuggest/Autosuggest.js
+++ react/fields/Autosuggest/Autosuggest.js
@@ -1,75 +1,24 @@
-import textfieldStyle from '../TextField/TextField.less';
-import autosuggestStyles from './Autosuggest.less';
+import styles from './Autosuggest.less';
 
 import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
 import ReactAutosuggest from 'react-autosuggest';
 import IsolatedScroll from 'react-isolated-scroll';
+import omit from 'lodash.omit';
 
-import ErrorIcon from '../../icons/ErrorIcon/ErrorIcon';
-import ClearField from '../TextField/ClearField/ClearField';
-
-function combineClassNames(props = {}, ...classNames) {
-  const { className, ...restProps } = props;
-
-  return {
-    className: classnames.apply(null, [...classNames, className]), // eslint-disable-line no-useless-call
-    ...restProps
-  };
-}
+import TextField from '../TextField/TextField';
 
 export default class Autosuggest extends Component {
 
   static displayName = 'Autosuggest';
 
   static propTypes = {
-    /* eslint-disable consistent-return */
-    id: (props, propName, componentName) => {
-      const { id, label } = props;
-
-      if (typeof id !== 'string') {
-        return new Error(`Invalid prop \`id\` of type \`${typeof id}\` supplied to \`${componentName}\`, expected \`string\`.`);
-      }
-
-      if (label && !id) {
-        return new Error(`When ${componentName} has a \`label\`, it should also have an \`id\`.`);
-      }
-    },
-    /* eslint-enable consistent-return */
-    className: PropTypes.string,
-    invalid: PropTypes.bool,
+    id: PropTypes.object.isRequired,
+    inputProps: PropTypes.object.isRequired,
     label: PropTypes.string,
-    /* eslint-disable consistent-return */
-    labelProps: (props, propName, componentName) => {
-      const { id, label, labelProps } = props;
-      const { htmlFor: labelFor } = labelProps || {};
-
-      if (typeof labelProps !== 'undefined' && typeof labelProps !== 'object') {
-        return new Error(`Invalid prop \`labelProps\` of type \`${typeof labelProps}\` supplied to \`${componentName}\`, expected \`object\`.`);
-      }
-
-      if (!label && labelProps) {
-        return new Error(`Specifying \`labelProps\` is redundant when \`label\` is not specified in ${componentName}.`);
-      }
-
-      if (labelFor && id) {
-        return new Error(`\`labelProps.htmlFor\` will be overridden by \`id\` in ${componentName}. Please remove it.`);
-      }
-    },
-    inputProps: (props, propName, componentName) => {
-      const { id, inputProps } = props;
-      const { id: inputId } = inputProps || {};
-
-      if (typeof inputProps !== 'undefined' && typeof inputProps !== 'object') {
-        return new Error(`Invalid prop \`inputProps\` of type \`${typeof inputProps}\` supplied to \`${componentName}\`, expected \`object\`.`);
-      }
-
-      if (inputId && id) {
-        return new Error(`\`inputProps.id\` will be overridden by \`id\` in ${componentName}. Please remove it.`);
-      }
-    },
-    /* eslint-disable consistent-return */
+    className: PropTypes.string,
     autosuggestProps: PropTypes.object.isRequired,
+    /* eslint-disable consistent-return */
     suggestionsContainerClassName: (props, _, componentName) => {
       const { suggestionsContainerClassName, autosuggestProps } = props;
       const { theme } = autosuggestProps || {};
@@ -83,69 +32,26 @@ export default class Autosuggest extends Component {
       if (suggestionsContainer && suggestionsContainerClassName) {
         return new Error('\`suggestionsContainerClassName\` will be overridden by the \`suggestionsContainer\` class in autosuggestProps \`theme\`. Please remove it.');
       }
-    },
-    help: PropTypes.string,
-    helpProps: PropTypes.object,
-    message: PropTypes.string,
-    messageProps: PropTypes.object,
-    onClear: PropTypes.func
+    }
+    /* eslint-enable consistent-return */
   };
 
   static defaultProps = {
-    id: '',
     className: '',
-    invalid: false,
-    label: '',
-    help: '',
-    message: ''
+    label: ''
   };
 
   constructor() {
     super();
 
     this.storeInputReference = this.storeInputReference.bind(this);
-    this.renderLabel = this.renderLabel.bind(this);
-    this.renderInput = this.renderInput.bind(this);
-    this.renderHelp = this.renderHelp.bind(this);
-    this.renderMessage = this.renderMessage.bind(this);
-    this.renderIcon = this.renderIcon.bind(this);
-    this.handleMouseDownOnClear = this.handleMouseDownOnClear.bind(this);
+    this.renderInputComponent = this.renderInputComponent.bind(this);
   }
 
   storeInputReference(autosuggest) {
     if (autosuggest !== null) {
       this.input = autosuggest.input;
     }
-  }
-
-  handleMouseDownOnClear(event) {
-    const { onClear } = this.props;
-
-    if (typeof onClear === 'function') {
-      onClear(event);
-      this.input.focus();
-      event.preventDefault(); // https://developer.mozilla.org/en/docs/Web/API/HTMLElement/focus#Notes
-    }
-  }
-
-  renderLabel() {
-    const { label } = this.props;
-
-    if (!label) {
-      return;
-    }
-
-    const { labelProps, id } = this.props;
-    const allLabelProps = {
-      ...combineClassNames(labelProps, textfieldStyle.label),
-      ...(id ? { htmlFor: id } : {})
-    };
-
-    return (
-      <label {...allLabelProps}>
-        {label}
-      </label>
-    );
   }
 
   renderSuggestionsContainer({ ref, ...rest }) {
@@ -160,121 +66,41 @@ export default class Autosuggest extends Component {
     );
   }
 
-  renderInput() {
-    const { inputProps, id, autosuggestProps, suggestionsContainerClassName } = this.props;
-    const { theme = {} } = autosuggestProps;
-    const { className: inputClassName, ...restInputProps } = inputProps;
+  renderInputComponent(inputProps) {
     const allInputProps = {
-      ...(id ? { id } : {}),
-      ...restInputProps
+      inputProps,
+      ...omit(this.props, [ 'inputProps', 'autosuggestProps' ])
     };
+
+    return (
+      <TextField {...allInputProps} />
+    );
+  }
+
+  render() {
+    const { inputProps, label, autosuggestProps, suggestionsContainerClassName } = this.props;
+    const { theme = {} } = autosuggestProps;
     const allAutosuggestProps = {
       renderSuggestionsContainer: this.renderSuggestionsContainer,
+      renderInputComponent: this.renderInputComponent,
       ...autosuggestProps,
       theme: {
-        ...autosuggestStyles,
+        ...styles,
         suggestionsContainer: classnames({
-          [autosuggestStyles.suggestionsContainer]: true,
+          [styles.suggestionsContainer]: true,
+          [styles.suggestionsContainer_withLabel]: label,
           [suggestionsContainerClassName]: suggestionsContainerClassName
         }),
-        ...theme,
-        input: classnames({
-          [textfieldStyle.input]: true,
-          [inputClassName]: inputClassName
-        })
+        ...theme
       }
     };
 
     return (
       <ReactAutosuggest
-        inputProps={allInputProps}
+        inputProps={inputProps}
         ref={this.storeInputReference}
         {...allAutosuggestProps}
       />
-    );
-  }
-
-  renderClear() {
-    return (
-      <span
-        className={textfieldStyle.clearField}
-        onMouseDown={this.handleMouseDownOnClear}
-        onTouchStart={this.handleMouseDownOnClear}>
-        <ClearField />
-      </span>
-    );
-  }
-
-  renderHelp() {
-    const { message, help } = this.props;
-
-    if (message || !help) {
-      return;
-    }
-
-    const { helpProps } = this.props;
-    const allHelpProps = combineClassNames(helpProps, textfieldStyle.help);
-
-    return (
-      <p {...allHelpProps}>
-        {help}
-      </p>
-    );
-  }
-
-  renderMessage() {
-    const { message } = this.props;
-
-    if (!message) {
-      return;
-    }
-
-    const { messageProps } = this.props;
-    const allMessageProps = combineClassNames(messageProps, textfieldStyle.message);
-
-    return (
-      <p {...allMessageProps}>
-        {this.renderIcon()}
-        {message}
-      </p>
-    );
-  }
-
-  renderIcon() {
-    const { invalid } = this.props;
-
-    if (!invalid) {
-      return;
-    }
-
-    return (
-      <ErrorIcon
-        filled={true}
-        className={textfieldStyle.messageIcon}
-        svgClassName={textfieldStyle.messageIconSvg}
-      />
-    );
-  }
-
-  render() {
-    const { className, invalid, onClear, inputProps = {} } = this.props;
-    const hasValue = (inputProps.value && inputProps.value.length > 0);
-    const canClear = hasValue && (typeof onClear === 'function');
-    const classNames = classnames({
-      [textfieldStyle.root]: true,
-      [textfieldStyle.invalid]: invalid,
-      [textfieldStyle.canClear]: canClear,
-      [className]: className
-    });
-
-    return (
-      <div className={classNames}>
-        {this.renderLabel()}
-        {this.renderInput()}
-        {this.renderClear()}
-        {this.renderHelp()}
-        {this.renderMessage()}
-      </div>
     );
   }
 
--- react/fields/Autosuggest/Autosuggest.test.js
+++ react/fields/Autosuggest/Autosuggest.test.js
@@ -4,18 +4,12 @@ import sinonChai from 'sinon-chai';
 import React from 'react';
 import {
   createRenderer,
-  Simulate,
   renderIntoDocument,
-  findRenderedDOMComponentWithClass,
   scryRenderedDOMComponentsWithClass
 } from 'react-addons-test-utils';
-import SyntheticEvent from 'react/lib/SyntheticEvent';
-import { findAllWithClass } from 'react-shallow-testutils';
 import Autosuggest from './Autosuggest';
 
 chai.use(sinonChai);
-
-const eventMatcher = sinon.match.instanceOf(SyntheticEvent);
 
 const renderer = createRenderer();
 
@@ -36,7 +30,7 @@ const getAutosuggestProps = (suggestions = []) => ({
 });
 
 describe('Autosuggest', () => {
-  let element, autosuggest, rootElement, label, input, help, message, messageIcon, clearField, errors, suggestion;
+  let element, autosuggest, errors, suggestion, suggestionsContainer;
 
   beforeEach(() => {
     errors = [];
@@ -53,32 +47,13 @@ describe('Autosuggest', () => {
   function render(jsx) {
     element = jsx;
     autosuggest = renderer.render(element);
-    label = findAllWithClass(autosuggest, 'label')[0] || null;
-    input = findAllWithClass(autosuggest, 'input')[0] || null;
-    help = findAllWithClass(autosuggest, 'help')[0] || null;
-    message = findAllWithClass(autosuggest, 'message')[0] || null;
-    messageIcon = findAllWithClass(autosuggest, 'messageIcon')[0] || null;
   }
 
   function renderToDom(jsx) {
     element = jsx;
     autosuggest = renderIntoDocument(element);
-    rootElement = scryRenderedDOMComponentsWithClass(autosuggest, 'root')[0];
-    input = findRenderedDOMComponentWithClass(autosuggest, 'input');
-    clearField = findRenderedDOMComponentWithClass(autosuggest, 'clearField');
     suggestion = scryRenderedDOMComponentsWithClass(autosuggest, 'suggestion');
-  }
-
-  function helpText() {
-    return help.props.children;
-  }
-
-  function messageText() {
-    return message.props.children[1];
-  }
-
-  function isClearButtonVisible() {
-    return /\bcanClear\b/.test(rootElement.className);
+    suggestionsContainer = scryRenderedDOMComponentsWithClass(autosuggest, 'suggestionsContainer')[0];
   }
 
   it('should have a displayName', () => {
@@ -86,214 +61,32 @@ describe('Autosuggest', () => {
     expect(element.type.displayName).to.equal('Autosuggest');
   });
 
-  describe('id', () => {
-    it('should error if `id` is not a string', () => {
-      render(<Autosuggest {...getAutosuggestProps()} id={true} />);
-      expect(errors[0]).to.match(/Invalid prop `id`/);
-    });
+  it('should render suggestions', () => {
+    renderToDom(<Autosuggest {...getAutosuggestProps(['test', 'test 2'])} />);
+    expect(suggestion).to.have.length(2);
   });
 
-  describe('label', () => {
-    it('should not be rendered by default', () => {
-      render(<Autosuggest {...getAutosuggestProps()} />);
-      expect(label).to.equal(null);
-    });
-
-    it('should have `htmlFor` equal to `id` when `label` is specified', () => {
-      render(<Autosuggest {...getAutosuggestProps()} id="firstName" label="First Name" />);
-      expect(label.props).to.contain.keys({ htmlFor: 'firstName' });
-    });
-
-    it('should have the right text when `label` is specified', () => {
-      render(<Autosuggest {...getAutosuggestProps()} id="firstName" label="First Name" />);
-      expect(label.props.children).to.equal('First Name');
-    });
-
-    it('should error if `id` is not specified but `label` is', () => {
-      render(<Autosuggest {...getAutosuggestProps()} label="First Name" />);
-      expect(errors[0]).to.match(/have an `id`/);
-    });
+  it('should render suggestions list below input field when a label is supplied', () => {
+    renderToDom(<Autosuggest {...getAutosuggestProps()} id="Foo" label="Foo" />);
+    expect(suggestionsContainer.className).to.contain('suggestionsContainer_withLabel');
   });
 
-  describe('labelProps', () => {
-    it('should error if `labelProps` is not an object', () => {
-      render(<Autosuggest {...getAutosuggestProps()} id="firstName" label="First Name" labelProps="data-automation=first-name" />);
-      expect(errors[0]).to.match(/Invalid prop `labelProps`/);
-    });
-
-    it('should error if `labelProps` is specified but `label` is not', () => {
-      render(<Autosuggest {...getAutosuggestProps()} labelProps={{ 'data-automation': 'first-name' }} />);
-      expect(errors[0]).to.match(/Specifying `labelProps` is redundant/);
-    });
-
-    it('should error if `labelProps`\'s `htmlFor` is specified', () => {
-      render(<Autosuggest {...getAutosuggestProps()} id="firstName" label="First Name" labelProps={{ htmlFor: 'ignored' }} />);
-      expect(errors[0]).to.match(/`labelProps.htmlFor` will be overridden by `id`/);
-    });
-
-    it('should pass through className to the label', () => {
-      render(<Autosuggest {...getAutosuggestProps()} id="firstName" label="First Name" labelProps={{ className: 'first-name-label' }} />);
-      expect(label.props.className).to.match(/first-name-label$/);
-    });
-
-    it('should pass through other props to the label', () => {
-      render(<Autosuggest {...getAutosuggestProps()} id="firstName" label="First Name" labelProps={{ 'data-automation': 'first-name-label' }} />);
-      expect(label.props['data-automation']).to.equal('first-name-label');
-    });
+  it('should render suggestions list below input field when a label is not supplied', () => {
+    renderToDom(<Autosuggest {...getAutosuggestProps()} />);
+    expect(suggestionsContainer.className).to.not.contain('suggestionsContainer_withLabel');
   });
 
-  describe('inputProps', () => {
-    it('should error if `inputProps` is not an object', () => {
-      render(<Autosuggest {...getAutosuggestProps()} inputProps="hey" />);
-      expect(errors[0]).to.match(/Invalid prop `inputProps`/);
-    });
-
-    it('should error if `inputProps`\'s `id` is specified', () => {
-      render(<Autosuggest {...getAutosuggestProps()} id="firstName" inputProps={{ id: 'ignored' }} />);
-      expect(errors[0]).to.match(/`inputProps.id` will be overridden by `id`/);
-    });
+  it('should error if `suggestionsContainerClassName` is not a string', () => {
+    render(<Autosuggest {...getAutosuggestProps()} suggestionsContainerClassName={true} />);
+    expect(errors[0]).to.match(/Invalid prop `suggestionsContainerClassName` of type `boolean` supplied to `Autosuggest`, expected `string/);
   });
 
-  describe('help', () => {
-    it('should not be rendered by default', () => {
-      render(<Autosuggest {...getAutosuggestProps()} />);
-      expect(help).to.equal(null);
-    });
-
-    it('should not be rendered when message exists', () => {
-      render(<Autosuggest {...getAutosuggestProps()} help="e.g. David" message="Something went wrong" />);
-      expect(help).to.equal(null);
-    });
-
-    it('should have the right text when `help` is specified', () => {
-      render(<Autosuggest {...getAutosuggestProps()} help="e.g. David" />);
-      expect(helpText()).to.equal('e.g. David');
-    });
-
-    it('should pass through className to the help text', () => {
-      render(<Autosuggest {...getAutosuggestProps()} help="e.g. David" helpProps={{ className: 'first-name-help' }} />);
-      expect(help.props.className).to.match(/first-name-help$/);
-    });
-
-    it('should pass through other props to the help text', () => {
-      render(<Autosuggest {...getAutosuggestProps()} help="e.g. David" helpProps={{ 'data-automation': 'first-name-help' }} />);
-      expect(help.props['data-automation']).to.equal('first-name-help');
-    });
-  });
-
-  describe('message', () => {
-    it('should not be rendered by default', () => {
-      render(<Autosuggest {...getAutosuggestProps()} />);
-      expect(message).to.equal(null);
-    });
-
-    it('should have the right text when `message` is specified', () => {
-      render(<Autosuggest {...getAutosuggestProps()} message="Something went wrong" />);
-      expect(messageText()).to.equal('Something went wrong');
-    });
-
-    it('should not render the message icon when `message` is specified and autosuggest is valid', () => {
-      render(<Autosuggest {...getAutosuggestProps()} message="Something went wrong" />);
-      expect(messageIcon).to.equal(null);
-    });
-
-    it('should pass through className to the message', () => {
-      render(<Autosuggest {...getAutosuggestProps()} message="Something went wrong" messageProps={{ className: 'first-name-message' }} />);
-      expect(message.props.className).to.match(/first-name-message$/);
-    });
-
-    it('should pass through other props to the message', () => {
-      render(<Autosuggest {...getAutosuggestProps()} message="Something went wrong" messageProps={{ 'data-automation': 'first-name-message' }} />);
-      expect(message.props['data-automation']).to.equal('first-name-message');
-    });
-  });
-
-  describe('invalid', () => {
-    it('should have the invalid className', () => {
-      render(<Autosuggest {...getAutosuggestProps()} invalid={true} />);
-      expect(autosuggest.props.className).to.contain('invalid');
-    });
-
-    it('should render a message icon', () => {
-      render(<Autosuggest {...getAutosuggestProps()} invalid={true} message="Something went wrong" />);
-      expect(messageIcon).not.to.equal(null);
-    });
-  });
-
-  describe('clear button', () => {
-    const handleBlur = sinon.spy();
-    const handleClear = sinon.spy();
-
-    const clickClear = () => {
-      Simulate.mouseDown(clearField);
-      Simulate.mouseUp(clearField);
-      Simulate.click(clearField);
+  it('should error if `suggestionsContainerClassName` and theme with class `suggestionsContainer` are both specified', () => {
+    const props = getAutosuggestProps();
+    props.autosuggestProps.theme = {
+      suggestionsContainer: 'TEST'
     };
-
-    beforeEach(() => {
-      handleBlur.reset();
-      handleClear.reset();
-    });
-
-    it('should not be visible when value is empty', () => {
-      renderToDom(<Autosuggest {...getAutosuggestProps()} inputProps={{ value: '' }} onClear={handleClear} />);
-      expect(isClearButtonVisible()).to.equal(false);
-    });
-
-    it('should be visible when value is provided', () => {
-      renderToDom(<Autosuggest {...getAutosuggestProps()} inputProps={{ value: 'abc' }} onClear={handleClear} />);
-      expect(isClearButtonVisible()).to.equal(true);
-    });
-
-    it('should be visible when value has white spaces only', () => {
-      renderToDom(<Autosuggest {...getAutosuggestProps()} inputProps={{ value: '  ' }} onClear={handleClear} />);
-      expect(isClearButtonVisible()).to.equal(true);
-    });
-
-    it('should not be visible when value is provided but no clear handler', () => {
-      renderToDom(<Autosuggest {...getAutosuggestProps()} inputProps={{ value: 'abc' }} />);
-      expect(isClearButtonVisible()).to.equal(false);
-    });
-
-    it('should invoke the clear handler when clicked', () => {
-      renderToDom(<Autosuggest {...getAutosuggestProps()} onClear={handleClear} />);
-      clickClear();
-      expect(handleClear.calledOnce).to.equal(true);
-      expect(handleClear).to.be.calledWith(eventMatcher);
-    });
-
-    it('should invoke the clear handler when touched', () => {
-      renderToDom(<Autosuggest {...getAutosuggestProps()} onClear={handleClear} />);
-      Simulate.touchStart(clearField);
-      expect(handleClear.calledOnce).to.equal(true);
-      expect(handleClear).to.be.calledWith(eventMatcher);
-    });
-
-    it('should focus the input when clicked', () => {
-      renderToDom(<Autosuggest {...getAutosuggestProps()} onClear={handleClear} />);
-      clickClear();
-      expect(global.document.activeElement).to.equal(input);
-    });
-  });
-
-  describe('autosuggest', () => {
-    it('should error if `suggestionsContainerClassName` is not a string', () => {
-      render(<Autosuggest {...getAutosuggestProps()} suggestionsContainerClassName={true} />);
-      expect(errors[0]).to.match(/Invalid prop `suggestionsContainerClassName` of type `boolean` supplied to `Autosuggest`, expected `string/);
-    });
-
-    it('should error if `suggestionsContainerClassName` and theme with class `suggestionsContainer` are both specified', () => {
-      const props = getAutosuggestProps();
-      props.autosuggestProps.theme = {
-        suggestionsContainer: 'TEST'
-      };
-      render(<Autosuggest {...props} suggestionsContainerClassName="TEST 2" />);
-      expect(errors[0]).to.match(/`suggestionsContainerClassName` will be overridden by the `suggestionsContainer` class in autosuggestProps `theme`. Please remove it./);
-    });
-
-    it('should render suggestions', () => {
-      renderToDom(<Autosuggest {...getAutosuggestProps(['test', 'test 2'])} />);
-      expect(suggestion).to.have.length(2);
-    });
+    render(<Autosuggest {...props} suggestionsContainerClassName="TEST 2" />);
+    expect(errors[0]).to.match(/`suggestionsContainerClassName` will be overridden by the `suggestionsContainer` class in autosuggestProps `theme`. Please remove it./);
   });
 });
