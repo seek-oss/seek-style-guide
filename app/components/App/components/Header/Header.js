6c5f2deb4dd5c6f0501f18399e17ddbcc4246974
Matt Jones <mjones@seek.com.au>
2016-10-20 14:38:17 +1100

    Fix field active styling

--- app/components/App/components/Header/Header.js
+++ app/components/App/components/Header/Header.js
@@ -2,10 +2,18 @@ import styles from './Header.less';
 
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
+import classnames from 'classnames';
 import { Logo } from 'seek-style-guide/react';
 import GridContainer from 'GridContainer/GridContainer';
 
+const fields = ['textfield', 'autosuggest'];
+
 export default function Header({ title }) {
+  const fieldsClassName = classnames({
+    [styles.menuItem]: true,
+    [styles.active]: fields.indexOf(title) > -1
+  });
+
   return (
     <GridContainer>
       <div className={styles.root}>
@@ -18,14 +26,14 @@ export default function Header({ title }) {
         <div>
           <Link className={styles.menuItem} activeClassName={styles.active} to="/" onlyActiveOnIndex={true}>Home</Link>
           <Link className={styles.menuItem} activeClassName={styles.active} to="typography">Typography</Link>
-          <Link className={styles.menuItem} activeClassName={styles.active} to="buttons">Buttons</Link>
-          <div className={styles.menuItem}>
-            <span>Fields</span>
+          <div className={fieldsClassName}>
+            Fields
             <ul className={styles.submenu}>
               <Link className={styles.link} to="textfield"><li>Textfield</li></Link>
               <Link className={styles.link} to="autosuggest"><li>Autosuggest</li></Link>
             </ul>
           </div>
+          <Link className={styles.menuItem} activeClassName={styles.active} to="buttons">Buttons</Link>
           <Link className={styles.menuItem} activeClassName={styles.active} to="icons">Icons</Link>
         </div>
       </div>
