fefbc8609270b1420b1721576108e8a689acbcf8
Todd <todd@toddm.me>
2016-11-08 11:14:42 +1100

    SeekApp Helmet refactor
    - Added the Helmet meta to SeekApp in the Style Guide and removed them from App.js in chalice

--- react/SeekApp/SeekApp.js
+++ react/SeekApp/SeekApp.js
@@ -2,15 +2,34 @@ import styles from './SeekApp.less';
 
 import React, { PropTypes } from 'react';
 import classnames from 'classnames';
+import Helmet from 'react-helmet';
+import ScreenReaderOnly from 'Accessibility/ScreenReaderOnly';
 
-export default function SeekApp({ fullScreen, children }) {
+const defaultPageTitleAU = 'SEEK - Australia\'s no. 1 jobs, employment, career and recruitment site';
+const defaultPageTitleNZ = 'Jobs on SEEK - New Zealand\'s no. 1 Employment, Career and Recruitment site';
+
+const getLocalisedPageTitle = locale => locale === 'AU' ? defaultPageTitleAU : defaultPageTitleNZ;
+
+export default function SeekApp({ fullScreen, children, meta = [], link = [], title, locale = 'AU' }) {
   const className = classnames({
     [styles.root]: true,
     [styles.fullScreen]: fullScreen
   });
 
+  const pageTitle = title || getLocalisedPageTitle(locale);
+
   return (
     <div className={className}>
+      <Helmet
+        title={pageTitle}
+        meta={meta}
+        link={link}
+      />
+
+      <ScreenReaderOnly>
+        <h1>{pageTitle}</h1>
+      </ScreenReaderOnly>
+
       {children}
     </div>
   );
@@ -18,7 +37,11 @@ export default function SeekApp({ fullScreen, children }) {
 
 SeekApp.propTypes = {
   fullScreen: PropTypes.bool,
-  children: PropTypes.node
+  children: PropTypes.node,
+  title: PropTypes.string,
+  meta: PropTypes.array,
+  link: PropTypes.array,
+  locale: PropTypes.oneOf(['AU', 'NZ'])
 };
 
 SeekApp.defaultProps = {
