4f3897931f0d7b67f1927849ae00f6bd59220537
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2017-02-20 16:43:01 +1100

    feat(layout): Add Card and Section groups (#129)

--- react/Card/Card.js
+++ react/Card/Card.js
@@ -2,13 +2,14 @@ import styles from './Card.less';
 import React, { PropTypes } from 'react';
 import classnames from 'classnames';
 
-export default function Card({ className, children, transparent, ...restProps }) {
+export default function Card({ className, children, group, transparent, ...restProps }) {
   return (
     <div
       {...restProps}
       className={classnames({
         [className]: className,
         [styles.root]: true,
+        [styles.group]: group,
         [styles.transparent]: transparent
       })}>
       {children}
@@ -19,5 +20,6 @@ export default function Card({ className, children, transparent, ...restProps })
 Card.propTypes = {
   className: PropTypes.string,
   children: PropTypes.node.isRequired,
+  group: PropTypes.bool,
   transparent: PropTypes.bool
 };
