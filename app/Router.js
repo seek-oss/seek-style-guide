9ed1fad40e184b5471ce5bf8aaf635e4672b3ce4
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2017-03-09 09:36:46 +1100

    WIP Remove old ‘app’ documentation folder

--- app/Router.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Router, browserHistory } from 'react-router';
-import routes from './Routes';
-
-export default function RouterComponent() {
-  return (
-    <Router routes={routes} history={browserHistory} />
-  );
-}
