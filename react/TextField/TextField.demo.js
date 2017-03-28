22b7ed78b72454202dbf7582b82a2645e9c8d0c8
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2017-03-28 15:30:17 +1100

    fix(TextField): Fix TextField clear icon
    The icon styles were adjusted via a class override, which causes rendering issues if the order of
    extracted CSS changes unexpectedly

--- react/TextField/TextField.demo.js
+++ react/TextField/TextField.demo.js
@@ -1,17 +1,68 @@
+import React, { Component, PropTypes } from 'react';
 import TextField from './TextField';
 import styles from './TextField.less';
 import classnames from 'classnames';
 import fieldMessageOptions from '../private/FieldMessage/FieldMessage.demo';
 import fieldLabelOptions from '../private/FieldLabel/FieldLabel.demo';
 
+class TextFieldContainer extends Component {
+  static propTypes = {
+    component: PropTypes.func.isRequired
+  };
+
+  constructor() {
+    super();
+
+    this.state = {
+      inputValue: ''
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      inputValue: event.target.value
+    });
+  };
+
+  handleClear = () => {
+    this.setState({
+      inputValue: ''
+    });
+  };
+
+  render() {
+    const { component: DemoComponent } = this.props;
+    const { inputValue } = this.state;
+
+    return (
+      <div style={{ width: '300px' }}>
+        <DemoComponent
+          inputProps={{
+            onChange: this.handleChange,
+            value: inputValue
+          }}
+          onClear={this.handleClear}
+        />
+      </div>
+    );
+  }
+}
+
 export default {
   route: '/textfields',
   title: 'Textfields',
   component: TextField,
+  container: TextFieldContainer,
   initialProps: {
     id: 'firstName',
     label: 'First Name',
-    message: 'e.g. Olivia'
+    message: 'e.g. Olivia',
+    // Documentation only:
+    inputProps: {
+      onChange: () => {},
+      value: '...'
+    },
+    onClear: () => {}
   },
   options: [
     {
