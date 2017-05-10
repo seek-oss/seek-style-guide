c6d57a1bc5404059df5cce7070a35a93a80ec3f7
Anna Kosolapova <kosanna.vipmail@gmail.com>
2017-05-11 09:46:28 +1000

    fix(logo): return relative logo so link does not overlay header (#189)

--- react/Header/Header.js
+++ react/Header/Header.js
@@ -43,7 +43,7 @@ export default function Header({
     <header className={styles.root} role="banner" aria-label="Primary navigation">
       <section className={styles.content}>
         <div className={styles.banner}>
-          <h1 data-automation="logo">
+          <h1 data-automation="logo" className={styles.logo}>
             <Logo svgClassName={styles.logoSvg} />
             {
               linkRenderer({
