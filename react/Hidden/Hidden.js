4eca1e69844c458487d31c028adc42f88dac4826
Anna Kosolapova <kosanna.vipmail@gmail.com>
2017-05-10 11:09:35 +1000

    feat(responsive): remove responsivness

--- react/Hidden/Hidden.js
+++ react/Hidden/Hidden.js
@@ -6,11 +6,6 @@ import styles from './Hidden.less';
 const Hidden = ({ children, xs, sm, md, lg, df, print, screen }) => (
   <span
     className={classNames({
-      [styles.xs]: xs,
-      [styles.sm]: sm,
-      [styles.md]: md,
-      [styles.lg]: lg,
-      [styles.df]: df,
       [styles.print]: print,
       [styles.screen]: screen,
     })}
@@ -19,21 +14,11 @@ const Hidden = ({ children, xs, sm, md, lg, df, print, screen }) => (
 
 Hidden.propTypes = {
   children: PropTypes.node.isRequired,
-  xs: PropTypes.bool,
-  sm: PropTypes.bool,
-  md: PropTypes.bool,
-  lg: PropTypes.bool,
-  df: PropTypes.bool,
   print: PropTypes.bool,
   screen: PropTypes.bool,
 };
 
 Hidden.defaultProps = {
-  xs: false,
-  sm: false,
-  md: false,
-  lg: false,
-  df: false,
   print: false,
   screen: false,
 };
--- react/Hidden/Hidden.test.js
+++ react/Hidden/Hidden.test.js
@@ -12,22 +12,6 @@ describe('components/grid/hidden', () => {
     ).toMatchSnapshot();
   });
 
-  it('should render with xs, sm, md', () => {
-    expect(
-      renderer.create(
-        <Hidden xs sm md>Text</Hidden>
-      ).toJSON()
-    ).toMatchSnapshot();
-  });
-
-  it('should render with lg, df', () => {
-    expect(
-      renderer.create(
-        <Hidden lg df>Text</Hidden>
-      ).toJSON()
-    ).toMatchSnapshot();
-  });
-
   it('should render with screen', () => {
     expect(
       renderer.create(
