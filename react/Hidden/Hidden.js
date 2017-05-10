56d5c18953cb4623803ec626047b25ce69cb98f8
Anna Kosolapova <kosanna.vipmail@gmail.com>
2017-05-10 14:14:54 +1000

    feat(Hidden): add className prop (#187)

--- react/Hidden/Hidden.js
+++ react/Hidden/Hidden.js
@@ -3,21 +3,26 @@ import classNames from 'classnames';
 
 import styles from './Hidden.less';
 
-const Hidden = ({ children, print, screen }) => (
+const Hidden = ({ children, className, print, screen }) => (
   <span
-    className={classNames({
-      [styles.print]: print,
-      [styles.screen]: screen
-    })}>{children}</span>
+    className={classNames(
+      className,
+      {
+        [styles.print]: print,
+        [styles.screen]: screen
+      }
+    )}>{children}</span>
 );
 
 Hidden.propTypes = {
   children: PropTypes.node.isRequired,
+  className: PropTypes.string,
   print: PropTypes.bool,
   screen: PropTypes.bool
 };
 
 Hidden.defaultProps = {
+  className: '',
   print: false,
   screen: false
 };
