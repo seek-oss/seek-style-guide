de90547fd92ef3db311476d704f38ee87e60f2d9
Anna Kosolapova <kosanna.vipmail@gmail.com>
2017-05-10 11:59:53 +1000

    feat(Hidden): Add Hidden component (#186)

---
+++ react/Hidden/Hidden.js
@@ -0,0 +1,25 @@
+import React, { PropTypes } from 'react';
+import classNames from 'classnames';
+
+import styles from './Hidden.less';
+
+const Hidden = ({ children, print, screen }) => (
+  <span
+    className={classNames({
+      [styles.print]: print,
+      [styles.screen]: screen
+    })}>{children}</span>
+);
+
+Hidden.propTypes = {
+  children: PropTypes.node.isRequired,
+  print: PropTypes.bool,
+  screen: PropTypes.bool
+};
+
+Hidden.defaultProps = {
+  print: false,
+  screen: false
+};
+
+export default Hidden;
---
+++ react/Hidden/Hidden.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Hidden from './Hidden';
+
+describe('components/grid/hidden', () => {
+  it('should render with children', () => {
+    expect(
+      renderer.create(
+        <Hidden>Text</Hidden>
+      ).toJSON()
+    ).toMatchSnapshot();
+  });
+
+  it('should render with screen', () => {
+    expect(
+      renderer.create(
+        <Hidden screen>Text</Hidden>
+      ).toJSON()
+    ).toMatchSnapshot();
+  });
+
+  it('should render with print', () => {
+    expect(
+      renderer.create(
+        <Hidden print>Text</Hidden>
+      ).toJSON()
+    ).toMatchSnapshot();
+  });
+});
