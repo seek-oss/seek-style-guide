8027ac9adc5f557279e8177d362f860e6d7ecb17
George Spyropoulos <gspyropoulos@seek.com.au>
2017-05-15 13:14:26 +1000

    feat(SlideToggle): Add tests

---
+++ react/SlideToggle/SlideToggle.test.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {
+  Simulate,
+  findRenderedDOMComponentWithTag,
+  renderIntoDocument
+} from 'react-dom/test-utils';
+import sinon from 'sinon';
+
+import SlideToggle from './SlideToggle';
+
+const renderSlideToggle = (props, children) => renderer.create(
+  <SlideToggle
+    id="testToggle"
+    text="Test toggle"
+    {...props}>
+    {children}
+  </SlideToggle>
+);
+
+describe('Slide toggle:', () => {
+  it('should render with default props', () => {
+    expect(renderSlideToggle().toJSON()).toMatchSnapshot();
+  });
+
+  it('should render a checked state', () => {
+    expect(renderSlideToggle({ checked: true }).toJSON()).toMatchSnapshot();
+  });
+
+  it('should render with the label on the left', () => {
+    expect(renderSlideToggle({ position: 'left' }).toJSON()).toMatchSnapshot();
+  });
+
+  it('should render with the label hidden', () => {
+    expect(renderSlideToggle({ hideLabel: true }).toJSON()).toMatchSnapshot();
+  });
+
+  it('should render child elements', () => {
+    expect(renderSlideToggle(null, <span>child</span>).toJSON()).toMatchSnapshot();
+  });
+
+  it('should render child elements in the left position', () => {
+    expect(renderSlideToggle({ position: 'left' }, <span>child</span>).toJSON()).toMatchSnapshot();
+  });
+
+  it('should call a passed in onToggle function when the checkbox fires a change event', () => {
+    const onToggle = jest.fn();
+    const slideToggle = renderIntoDocument(
+      <SlideToggle
+        id="testToggle"
+        text="Test toggle"
+        onToggle={onToggle}
+      />
+    );
+    const input = findRenderedDOMComponentWithTag(slideToggle, 'input');
+    Simulate.change(input);
+    expect(onToggle).toHaveBeenCalledWith(false);
+  });
+});
