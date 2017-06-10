72a3ff9d36fe554e2394bec15c023d7b91dc717c
Kosanna <kosanna.vipmail@gmail.com>
2017-06-10 23:00:22 +1000

    feat(Enzyme): Add enzyme and enzyme-to-json, cleanup snapshot tests

--- react/Header/Header.test.js
+++ react/Header/Header.test.js
@@ -1,48 +1,48 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 import Header from './Header';
 
-const renderHeader = props => renderer.create(<Header {...props} />);
+const renderHeader = props => shallow(<Header {...props} />);
 
 describe('Header:', () => {
   it('should render with locale of AU', () => {
-    expect(renderHeader({ locale: 'AU' }).toJSON()).toMatchSnapshot();
+    expect(renderHeader({ locale: 'AU' })).toMatchSnapshot();
   });
 
   it('should render with locale of NZ', () => {
-    expect(renderHeader({ locale: 'NZ' }).toJSON()).toMatchSnapshot();
+    expect(renderHeader({ locale: 'NZ' })).toMatchSnapshot();
   });
 
   it('should render when authenticated', () => {
-    expect(renderHeader({ authenticationStatus: 'authenticated', userName: 'Leeroy', userEmail: 'leeroybrown@email.com' }).toJSON()).toMatchSnapshot();
+    expect(renderHeader({ authenticationStatus: 'authenticated', userName: 'Leeroy', userEmail: 'leeroybrown@email.com' })).toMatchSnapshot();
   });
 
   it('should render when authenticated but username and email is not yet provided', () => {
-    expect(renderHeader({ authenticationStatus: 'authenticated', userName: '', userEmail: '' }).toJSON()).toMatchSnapshot();
+    expect(renderHeader({ authenticationStatus: 'authenticated', userName: '', userEmail: '' })).toMatchSnapshot();
   });
 
   it('should render first part of email address when username isn\'t present', () => {
-    expect(renderHeader({ authenticationStatus: 'authenticated', userName: '', userEmail: 'leeroybrown@email.com' }).toJSON()).toMatchSnapshot();
+    expect(renderHeader({ authenticationStatus: 'authenticated', userName: '', userEmail: 'leeroybrown@email.com' })).toMatchSnapshot();
   });
 
   it('should render when unauthenticated', () => {
-    expect(renderHeader({ authenticationStatus: 'unauthenticated' }).toJSON()).toMatchSnapshot();
+    expect(renderHeader({ authenticationStatus: 'unauthenticated' })).toMatchSnapshot();
   });
 
   it('should render when authentication is pending', () => {
-    expect(renderHeader({ authenticationStatus: 'pending' }).toJSON()).toMatchSnapshot();
+    expect(renderHeader({ authenticationStatus: 'pending' })).toMatchSnapshot();
   });
 
   it('should render when activeTab is \'Profile\'', () => {
-    expect(renderHeader({ activeTab: 'Profile' }).toJSON()).toMatchSnapshot();
+    expect(renderHeader({ activeTab: 'Profile' })).toMatchSnapshot();
   });
 
   it('should render with no divider', () => {
-    expect(renderHeader({ divider: false }).toJSON()).toMatchSnapshot();
+    expect(renderHeader({ divider: false })).toMatchSnapshot();
   });
 
   it('should append returnUrl to signin and register links if present', () => {
-    expect(renderHeader({ returnUrl: '/jobs' }).toJSON()).toMatchSnapshot();
+    expect(renderHeader({ returnUrl: '/jobs' })).toMatchSnapshot();
   });
 });
