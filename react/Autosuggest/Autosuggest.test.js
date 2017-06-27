ce4be1eaaea939f3dc847d190e58befe5fda0925
Kosanna <kosanna.vipmail@gmail.com>
2017-06-27 14:53:28 +1000

    Increase autosuggest field height, cleanup tests

--- react/Autosuggest/Autosuggest.test.js
+++ react/Autosuggest/Autosuggest.test.js
@@ -1,19 +1,7 @@
-import chai, { expect } from 'chai';
-import sinon from 'sinon';
-import sinonChai from 'sinon-chai';
+import { mount, render } from 'enzyme';
 import React from 'react';
-import {
-  renderIntoDocument,
-  scryRenderedDOMComponentsWithClass,
-  findRenderedDOMComponentWithClass,
-  Simulate
-} from 'react-dom/test-utils';
-import { createRenderer } from 'react-test-renderer/shallow';
+
 import Autosuggest from './Autosuggest';
-
-chai.use(sinonChai);
-
-const renderer = createRenderer();
 
 const getAutosuggestProps = (suggestions = []) => ({
   autosuggestProps: {
@@ -32,56 +20,36 @@ const getAutosuggestProps = (suggestions = []) => ({
 });
 
 describe('Autosuggest', () => {
-  let element, autosuggest, errors, suggestions, suggestionsContainer, input;
-
   beforeEach(() => {
-    errors = [];
-
-    sinon.stub(console, 'error', errorMessage => {
-      errors.push(errorMessage);
-    });
+    global.console = { error: jest.fn() };
   });
 
   afterEach(() => {
-    console.error.restore();
+    jest.clearAllMocks();
   });
 
-  function render(jsx) {
-    element = jsx;
-    autosuggest = renderer.render(element);
-  }
-
-  function renderToDom(jsx) {
-    element = jsx;
-    autosuggest = renderIntoDocument(element);
-    suggestions = scryRenderedDOMComponentsWithClass(autosuggest, 'suggestion');
-    suggestionsContainer = scryRenderedDOMComponentsWithClass(autosuggest, 'suggestionsContainer')[0];
-    input = findRenderedDOMComponentWithClass(autosuggest, 'input');
-  }
-
-  it('should have a displayName', () => {
-    render(<Autosuggest {...getAutosuggestProps()} />);
-    expect(element.type.displayName).to.equal('Autosuggest');
+  it('should render with simple props', () => {
+    const wrapper = render(<Autosuggest {...getAutosuggestProps()} />);
+    expect(wrapper).toMatchSnapshot();
   });
 
-  it('should render suggestions', () => {
-    renderToDom(<Autosuggest {...getAutosuggestProps(['test', 'test 2'])} />);
-    expect(suggestions).to.have.length(2);
+  it('should render with suggestions', () => {
+    const wrapper = render(<Autosuggest {...getAutosuggestProps(['test', 'test 2'])} />);
+    const suggestions = wrapper.find('.suggestionsContainer').find('li');
+    expect(wrapper).toMatchSnapshot();
+    expect(suggestions.length).toEqual(2);
   });
 
-  it('should render suggestions list below input field when a label is supplied', () => {
-    renderToDom(<Autosuggest {...getAutosuggestProps()} id="Foo" label="Foo" />);
-    expect(suggestionsContainer.className).to.contain('suggestionsContainer_withLabel');
-  });
-
-  it('should render suggestions list below input field when a label is not supplied', () => {
-    renderToDom(<Autosuggest {...getAutosuggestProps()} />);
-    expect(suggestionsContainer.className).to.not.contain('suggestionsContainer_withLabel');
+  it('should render with label', () => {
+    const wrapper = render(<Autosuggest {...getAutosuggestProps()} id="Foo" label="Foo" />);
+    expect(wrapper).toMatchSnapshot();
   });
 
   it('should error if `suggestionsContainerClassName` is not a string', () => {
+    const expectedError = expect.stringMatching(/Invalid prop `suggestionsContainerClassName` of type `boolean` supplied to `Autosuggest`, expected `string/);
     render(<Autosuggest {...getAutosuggestProps()} suggestionsContainerClassName={true} />);
-    expect(errors[0]).to.match(/Invalid prop `suggestionsContainerClassName` of type `boolean` supplied to `Autosuggest`, expected `string/);
+
+    expect(console.error).toBeCalledWith(expectedError);
   });
 
   it('should error if `suggestionsContainerClassName` and theme with class `suggestionsContainer` are both specified', () => {
@@ -89,13 +57,17 @@ describe('Autosuggest', () => {
     props.autosuggestProps.theme = {
       suggestionsContainer: 'TEST'
     };
+    const expectedError = expect.stringMatching(/`suggestionsContainerClassName` will be overridden by the `suggestionsContainer` class in autosuggestProps `theme`. Please remove it./);
+
     render(<Autosuggest {...props} suggestionsContainerClassName="TEST 2" />);
-    expect(errors[0]).to.match(/`suggestionsContainerClassName` will be overridden by the `suggestionsContainer` class in autosuggestProps `theme`. Please remove it./);
+    expect(console.error).toBeCalledWith(expectedError);
   });
 
   it('should focus field when suggestion is clicked', () => {
-    renderToDom(<Autosuggest {...getAutosuggestProps(['test', 'test 2'])} />);
-    Simulate.click(suggestions[0]);
-    expect(global.document.activeElement).to.equal(input);
+    const wrapper = mount(<Autosuggest {...getAutosuggestProps(['test', 'test 2'])} />);
+    const firstSuggestion = wrapper.find('li').first();
+    const input = wrapper.find('input').html();
+    firstSuggestion.simulate('click');
+    expect(global.document.activeElement.outerHTML).toEqual(input);
   });
 });
