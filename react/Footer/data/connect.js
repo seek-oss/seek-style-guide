76a584a23e92bf0430ac2bcfaad94651e645ee15
Aden <adennn@gmail.com>
2017-06-29 15:43:14 +1000

    fix(Footer): Remove Product & Tech blog link (#225)
    Product & Tech blog is about to be retired

--- react/Footer/data/connect.js
+++ react/Footer/data/connect.js
@@ -9,12 +9,6 @@ export default [
     href: '/work-for-seek',
     analytics: 'toolbar:work+for+seek',
     secondary: true
-  },
-  {
-    name: 'Product & Tech Blog',
-    href: 'http://productblog.seek.com.au',
-    analytics: 'toolbar:product+blog',
-    secondary: true
   }
 ];
 
