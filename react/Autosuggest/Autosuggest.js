b3be6e9b33cb450cde4c5a7692f222c278f9a493
Kosanna <kosanna.vipmail@gmail.com>
2017-07-19 16:21:28 +1000

    Spread the imports

--- react/Autosuggest/Autosuggest.js
+++ react/Autosuggest/Autosuggest.js
@@ -5,7 +5,10 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import ReactAutosuggest from 'react-autosuggest';
 import IsolatedScroll from 'react-isolated-scroll';
-import { get, invoke, omit } from 'lodash';
+
+import get from 'lodash/get';
+import invoke from 'lodash/invoke';
+import omit from 'lodash/omit';
 
 import TextField from '../TextField/TextField';
 
