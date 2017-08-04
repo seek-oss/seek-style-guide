289aa96e55c4f5989469c230cc17d5713a35a6f6
SEEKASIA\zhenyong.chew <zhenyong.chew@seekasia.com>
2017-08-04 14:19:52 +0800

    build(multiTenant): Initial code.

---
+++ react/Button/Button.css.js
@@ -0,0 +1,13 @@
+import {buttonPrimary, buttonDefault, buttonSuccess} from '../../theme/palette/palette.js';
+
+export default {
+	'.primary': {
+		backgroundColor: buttonPrimary
+	},
+	'.default': {
+		backgroundColor: buttonDefault
+	},
+	'.success': {
+		backgroundColor: buttonSuccess
+	}
+}
