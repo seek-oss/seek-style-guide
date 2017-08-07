8b68788504bc787b9aa2ebb901fdc8beb065f57b
SEEKASIA\zhenyong.chew <zhenyong.chew@seekasia.com>
2017-08-07 17:36:07 +0800

    Revert "Revert "Change header style based on tenant.""
    This reverts commit 4c01f44ff1f182f3640f37f0536d41c40774e037.

--- theme/palette/palette.js
+++ theme/palette/palette.js
@@ -1,19 +1,37 @@
-export const saBlue = '#0D3880';
-	
-let brand = 'jobst';
+let tenant = process.env.SKU_TENANT;
 
+//default SeekAsia colors
 let primary = '#0D3880';
-let tempDefault = '#E60278';
+let secondary = '#E60278';
+let tempDefault = '#D6D6D6';
 let success = '#157E00';
 
-if(brand == 'jobst') {
+let headerBackgroundColor = primary;
+let headerFontColor = '#FFF';
+
+if(tenant == 'jobStreet') {
 	primary = '#FFF200';
+	secondary = '#1C3F94';
 	tempDefault = '#DDDDDD';
 	success = '#18CA6C';
+
+	headerBackgroundColor = secondary;
+	headerFontColor = '#FFF';
+} else if(tenant == 'jobsDB') {
+	primary = '#FFF200';
+	secondary = '#1C3F94';
+	tempDefault = '#DDDDDD';
+	success = '#18CA6C';
+
+	headerBackgroundColor = '#FFF';
+	headerFontColor = '#298EB9';
 }
 
 
 export const buttonPrimary = primary;
 export const buttonDefault = tempDefault;
 export const buttonSuccess = success;
+
+export const headerBackground = headerBackgroundColor;
+export const headerColor = headerFontColor;
 
