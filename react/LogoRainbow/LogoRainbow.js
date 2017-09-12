4497c5231135b11d7bb1f551a946a19ea401b4e3
Todd Moore <todd@toddm.me>
2017-09-12 12:29:30 +1000

    feat(RainbowLogo): Custom Marriage Equality logo

---
+++ react/LogoRainbow/LogoRainbow.js
@@ -0,0 +1,64 @@
+import styles from './LogoRainbow.less';
+import LogoRainbowText from './LogoRainbowText';
+import LogoRainbowIcon from './LogoRainbowIcon';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import classnames from 'classnames';
+
+const viewportDimensions = type => ({
+  default: { height: 68.031, width: 170.079 },
+  compact: { height: 68.031, width: 68.031 }
+})[type];
+
+const getViewPortDimension = (type, dimension) => viewportDimensions(type)[dimension];
+
+export default function LogoRainbow({
+  svgClassName,
+  invert,
+  compact,
+  textClass,
+  ...restProps }) {
+  const svgClasses = classnames(svgClassName, {
+    [styles.root]: true,
+    [styles.invert]: invert
+  });
+  const type = compact && 'compact' || 'default';
+  const height = getViewPortDimension(type, 'height');
+  const width = getViewPortDimension(type, 'width');
+
+  return (
+    <div {...restProps}>
+      <svg
+        className={svgClasses}
+        id="sk-logo-pos"
+        xmlns="http://www.w3.org/2000/svg"
+        height={`${height}`}
+        viewBox={`0 0 ${width} ${height}`}
+        width={`${width}px`}
+        y="0px"
+        x="0px">
+        { !compact ?
+          <LogoRainbowText textClass={invert && styles.invertedLogoText || textClass} /> :
+          null
+        }
+        <LogoRainbowIcon />
+      </svg>
+    </div>
+  );
+}
+
+LogoRainbow.propTypes = {
+  svgClassName: PropTypes.string,
+  invert: PropTypes.bool,
+  compact: PropTypes.bool,
+  className: PropTypes.string,
+  textClass: PropTypes.string
+};
+
+LogoRainbow.defaultProps = {
+  svgClassName: '',
+  className: '',
+  textClass: styles.logoText
+};
