b325fefe512f64c90bb30c62e02a244b7db0c9ab
Cindy On Show <conshow@seek.com.au>
2017-09-15 13:13:19 +1000

    fix(standalone): revert rainbow logo for standalone app

--- standalone/header-footer/src/client.js
+++ standalone/header-footer/src/client.js
@@ -1,9 +1,8 @@
 import makeStandalone from './makeStandalone';
 import Header from 'seek-style-guide/react/Header/Header';
 import Footer from 'seek-style-guide/react/Footer/Footer';
-import LogoRainbow from 'seek-style-guide/react/LogoRainbow/LogoRainbow';
 
-export const renderHeader = makeStandalone(Header, { logoComponent: LogoRainbow });
+export const renderHeader = makeStandalone(Header);
 export const renderFooter = makeStandalone(Footer);
 
 // Allow standalone header consumers to create React elements
