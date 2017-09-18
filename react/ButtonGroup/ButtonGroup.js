5309f160115dd22ff5fec2937b7855660e3e0917
Devon Borysiewicz <devon.borysiewicz@gmail.com>
2017-09-18 15:20:50 +1000

    WIP: Added ability to pass className into ButtonGroup

--- react/ButtonGroup/ButtonGroup.js
+++ react/ButtonGroup/ButtonGroup.js
@@ -2,15 +2,18 @@ import styles from '../Button/Button.less';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function ButtonGroup({ children, ...restProps }) {
+import classnames from 'classnames';
+
+export default function ButtonGroup({ className, children, ...restProps }) {
   return (
-    <div {...restProps} className={styles.group}>
+    <div {...restProps} className={classnames({ [className]: className, [styles.group]: true })}>
       {children}
     </div>
   );
 }
 
 ButtonGroup.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
