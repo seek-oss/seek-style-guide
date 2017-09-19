07b2706c60f3553cc1f977254e2eb2b912d2e110
Devon Borysiewicz <devon.borysiewicz@gmail.com>
2017-09-19 14:02:13 +1000

    WIP: Changed to test to stop firing warnings

--- react/ButtonGroup/ButtonGroup.test.js
+++ react/ButtonGroup/ButtonGroup.test.js
@@ -4,13 +4,13 @@ import ButtonGroup from './ButtonGroup';
 import Button from '../Button/Button';
 
 describe('ButtonGroup:', () => {
-  it('should render a button group', () => {
-    const wrapper = shallow(<ButtonGroup />);
+  it('should render a button group with node', () => {
+    const wrapper = shallow(<ButtonGroup><Button color="blue"><h5>SEEK</h5></Button></ButtonGroup>);
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should render with node', () => {
-    const wrapper = shallow(<ButtonGroup><Button color="blue"><h5>SEEK</h5></Button></ButtonGroup>);
+  it('should combine className when passed in', () => {
+    const wrapper = shallow(<ButtonGroup className="combineMe"><Button color="blue"><h5>SEEK</h5></Button></ButtonGroup>);
     expect(wrapper).toMatchSnapshot();
   });
 });
