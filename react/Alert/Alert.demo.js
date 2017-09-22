b3ea0a01fe79861cc570cc5e936ce96dc153f31d
George Spyropoulos <gspyropoulos@seek.com.au>
2017-09-22 11:48:56 +1000

    fix(Alert): First cut of styling for alerts

--- react/Alert/Alert.demo.js
+++ react/Alert/Alert.demo.js
@@ -1,14 +1,73 @@
-// import React from 'react';
 import { Alert } from 'seek-style-guide/react';
-// import styles from './Alert.less';
-// import classnames from 'classnames';
+import { LEVEL, TYPE } from './Alert';
 
 export default {
   route: '/alert',
   title: 'Alert',
   component: Alert,
   initialProps: {
+    type: TYPE.POSITIVE,
+    level: LEVEL.PRIMARY,
     message: 'Test message text lorem ipsum dolor set amet'
   },
-  options: []
+  options: [{
+    label: 'Type',
+    type: 'radio',
+    states: [
+      {
+        label: 'Positive',
+        transformProps: props => ({
+          ...props,
+          type: TYPE.POSITIVE
+        })
+      },
+      {
+        label: 'Info',
+        transformProps: props => ({
+          ...props,
+          type: TYPE.INFO
+        })
+      },
+      {
+        label: 'Critical',
+        transformProps: props => ({
+          ...props,
+          type: TYPE.CRITICAL
+        })
+      },
+      {
+        label: 'Help',
+        transformProps: props => ({
+          ...props,
+          type: TYPE.HELP
+        })
+      }
+    ]
+  }, {
+    label: 'Level',
+    type: 'radio',
+    states: [
+      {
+        label: 'Primary',
+        transformProps: props => ({
+          ...props,
+          level: LEVEL.PRIMARY
+        })
+      },
+      {
+        label: 'Secondary',
+        transformProps: props => ({
+          ...props,
+          level: LEVEL.SECONDARY
+        })
+      },
+      {
+        label: 'Tertiary',
+        transformProps: props => ({
+          ...props,
+          level: LEVEL.TERTIARY
+        })
+      }
+    ]
+  }]
 };
--- react/Alert/Alert.js
+++ react/Alert/Alert.js
@@ -2,29 +2,59 @@ import styles from './Alert.less';
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import Text from '../Text/Text';
+import TickCircleIcon from '../TickCircleIcon/TickCircleIcon';
 import InfoIcon from '../InfoIcon/InfoIcon';
+import ErrorIcon from '../ErrorIcon/ErrorIcon';
+import HelpIcon from '../HelpIcon/HelpIcon';
+
+export const TYPE = {
+  POSITIVE: 'positive',
+  INFO: 'info',
+  CRITICAL: 'critical',
+  HELP: 'help'
+};
+
+export const LEVEL = {
+  PRIMARY: 'primary',
+  SECONDARY: 'secondary',
+  TERTIARY: 'tertiary'
+};
+
+const ICONS = {
+  [TYPE.POSITIVE]: TickCircleIcon,
+  [TYPE.INFO]: InfoIcon,
+  [TYPE.CRITICAL]: ErrorIcon,
+  [TYPE.HELP]: HelpIcon
+};
 
 export default class Alert extends Component {
 
   static displayName = 'Alert';
 
   static propTypes = {
+    type: PropTypes.oneOf([TYPE.POSITIVE, TYPE.INFO, TYPE.CRITICAL, TYPE.HELP]),
+    level: PropTypes.oneOf([LEVEL.PRIMARY, LEVEL.SECONDARY, LEVEL.TERTIARY]),
     message: PropTypes.string.isRequired
   };
 
-  static defaultProps = {};
+  static defaultProps = {
+    type: TYPE.INFO,
+    level: LEVEL.SECONDARY
+  };
 
   render() {
-    const { message } = this.props;
+    const { message, type, level } = this.props;
+    const Icon = ICONS[type];
 
     return (
-      <div className={styles.root}>
-        <InfoIcon className={styles.icon} svgClassName={styles.iconSvg} />
-        <Text className={styles.text} raw baseline={false}>
-          {message}
-        </Text>
+      <div className={classnames(styles.root, styles[type], styles[level])}>
+        <Icon className={styles.icon} />
+        <div className={styles.text}>
+          <Text raw baseline={false}>{message}</Text>
+        </div>
       </div>
     );
   }
