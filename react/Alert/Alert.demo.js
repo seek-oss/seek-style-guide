693227dbcf02bf7df659ad0ceffbc64c713bdc14
George Spyropoulos <gspyropoulos@seek.com.au>
2017-09-25 17:05:11 +1000

    fix(Alert): More alert styling

--- react/Alert/Alert.demo.js
+++ react/Alert/Alert.demo.js
@@ -1,16 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Alert } from 'seek-style-guide/react';
+import { Card, Section } from 'seek-style-guide/react';
 import { LEVEL, TYPE } from './Alert';
+
+const Container = ({ component: DemoComponent, componentProps }) => (
+  <Card style={{ width: '500px' }}>
+    <Section>
+      <DemoComponent {...componentProps} />
+    </Section>
+  </Card>
+);
+
+Container.propTypes = {
+  component: PropTypes.any,
+  componentProps: PropTypes.object.isRequired
+};
 
 export default {
   route: '/alert',
   title: 'Alert',
   component: Alert,
+  container: Container,
   initialProps: {
     type: TYPE.POSITIVE,
     level: LEVEL.PRIMARY,
-    message: 'Test message text lorem ipsum dolor set amet'
+    message: 'I\'m some timely text that helps the user understand something',
+    hideIcon: false,
+    onClose: () => console.log('On close handler called')
   },
   options: [{
+    label: 'States',
+    type: 'checklist',
+    states: [
+      {
+        label: 'Hide icon',
+        transformProps: ({ hideIcon, ...props }) => ({
+          ...props,
+          hideIcon: !hideIcon
+        })
+      },
+      {
+        label: 'Show close button',
+        transformProps: ({ showCloseButton, ...props }) => ({
+          ...props,
+          showCloseButton: !showCloseButton
+        })
+      }
+    ]
+  },
+  {
     label: 'Type',
     type: 'radio',
     states: [
--- react/Alert/Alert.js
+++ react/Alert/Alert.js
@@ -9,6 +9,7 @@ import TickCircleIcon from '../TickCircleIcon/TickCircleIcon';
 import InfoIcon from '../InfoIcon/InfoIcon';
 import CriticalIcon from '../CriticalIcon/CriticalIcon';
 import HelpIcon from '../HelpIcon/HelpIcon';
+import CrossIcon from '../CrossIcon/CrossIcon';
 
 export const TYPE = {
   POSITIVE: 'positive',
@@ -37,24 +38,45 @@ export default class Alert extends Component {
   static propTypes = {
     type: PropTypes.oneOf([TYPE.POSITIVE, TYPE.INFO, TYPE.CRITICAL, TYPE.HELP]),
     level: PropTypes.oneOf([LEVEL.PRIMARY, LEVEL.SECONDARY, LEVEL.TERTIARY]),
-    message: PropTypes.string.isRequired
+    message: PropTypes.string.isRequired,
+    hideIcon: PropTypes.bool,
+    showCloseButton: PropTypes.bool,
+    onClose: PropTypes.func
   };
 
   static defaultProps = {
     type: TYPE.INFO,
-    level: LEVEL.SECONDARY
+    level: LEVEL.SECONDARY,
+    hideIcon: false,
+    showCloseButton: false
   };
 
+  handleClose = event => this.props.onClose(event);
+
   render() {
-    const { message, type, level } = this.props;
+    const { message, type, level, hideIcon, showCloseButton } = this.props;
+
+    const rootClasses = classnames({
+      [styles.root]: true,
+      [styles[type]]: true,
+      [styles[level]]: true,
+      [styles.hideIcon]: hideIcon,
+      [styles.showCloseButton]: showCloseButton
+    });
+
     const Icon = ICONS[type];
 
     return (
-      <div className={classnames(styles.root, styles[type], styles[level])}>
-        <Icon className={styles.icon} />
+      <div className={rootClasses}>
+        {!hideIcon && <Icon className={styles.icon} />}
         <div className={styles.text}>
           <Text raw baseline={false}>{message}</Text>
         </div>
+        {showCloseButton && (
+          <button className={styles.close} onClick={this.handleClose}>
+            <CrossIcon />
+          </button>
+        )}
       </div>
     );
   }
