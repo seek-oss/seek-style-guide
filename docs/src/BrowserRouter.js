0dd58fddf893a11a2c2595c2e99d8bf77b33fac4
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2017-10-06 21:54:34 +1100

    WIP Remove old component name

--- docs/src/BrowserRouter.js
+++ docs/src/BrowserRouter.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import App from 'App/App';
 
-export default function RouterComponent() {
-  return (
-    <BrowserRouter basename={process.env.BASE_HREF}>
-      <App />
-    </BrowserRouter>
-  );
-}
+export default () => (
+  <BrowserRouter basename={process.env.BASE_HREF}>
+    <App />
+  </BrowserRouter>
+);
