9eb7f1b600158251f93cef92e779e49efc883445
Anna Kosolapova <kosanna.vipmail@gmail.com>
2017-11-28 10:28:35 +1100

    feat(Button): Add ghost button styles (#377)
    This PR: 
    - adds a non-required `ghost` prop to the Button
    - adds styles to ghost blue buttons
    - adds style to ghost white button
    
    Example:
    
    blue
    <img width="159" alt="screen shot 2017-11-23 at 8 02 19 pm" src="https://user-images.githubusercontent.com/15971854/33165194-fa72348e-d089-11e7-9713-0b81d7b17ba0.png"><img width="159" alt="screen shot 2017-11-23 at 8 02 35 pm" src="https://user-images.githubusercontent.com/15971854/33165189-fa05e0cc-d089-11e7-86c2-f6348ee2273a.png"><img width="154" alt="screen shot 2017-11-23 at 8 02 44 pm" src="https://user-images.githubusercontent.com/15971854/33165186-f99833b0-d089-11e7-98cf-a06c4c8ee7f6.png">
    
    transparrent white
    <img width="175" alt="screen shot 2017-11-23 at 8 04 49 pm" src="https://user-images.githubusercontent.com/15971854/33165195-fac46556-d089-11e7-97e5-300a332d6fd1.png"><img width="164" alt="screen shot 2017-11-23 at 8 04 56 pm" src="https://user-images.githubusercontent.com/15971854/33165191-fa3db006-d089-11e7-864a-33540c305d32.png"><img width="157" alt="screen shot 2017-11-23 at 8 05 04 pm" src="https://user-images.githubusercontent.com/15971854/33165187-f9cfe36e-d089-11e7-9919-ca1a32d2f8c4.png">
    
    Example usage:
    ```
    <Button color="white" ghost {...restProps} />
    ```

--- react/Button/Button.js
+++ react/Button/Button.js
@@ -4,13 +4,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import classnames from 'classnames';
+import capitalize from 'lodash/capitalize';
 
 export default class Button extends Component {
   static displayName = 'Button';
 
   static propTypes = {
     color: PropTypes.oneOf([
-      'pink', 'blue', 'gray', 'transparent'
+      'pink', 'blue', 'gray', 'transparent', 'white'
     ]).isRequired,
     children: PropTypes.oneOfType([
       PropTypes.arrayOf(PropTypes.node),
@@ -21,40 +22,34 @@ export default class Button extends Component {
       PropTypes.func,
       PropTypes.string
     ]),
+    ghost: PropTypes.bool,
     loading: PropTypes.bool,
     fullWidth: PropTypes.bool
   };
 
   static defaultProps = {
     className: '',
+    ghost: false,
     loading: false,
     fullWidth: false,
     component: 'button'
   };
 
-  constructor() {
-    super();
-
-    this.storeButtonReference = this.storeButtonReference.bind(this);
-  }
-
-  storeButtonReference(button) {
+  storeButtonReference = button => {
     if (button !== null) {
       this.button = button;
     }
   }
 
   render() {
-    const { color, className, loading, fullWidth, children, component, ...restProps } = this.props;
+    const { color, ghost, className, loading, fullWidth, children, component, ...restProps } = this.props;
 
     const combinedProps = {
       className: classnames(styles.root, className, {
         [styles.loading]: loading,
         [styles.fullWidth]: fullWidth,
-        [styles.root_isPink]: color === 'pink',
-        [styles.root_isBlue]: color === 'blue',
-        [styles.root_isGray]: color === 'gray',
-        [styles.root_isTransparent]: color === 'transparent'
+        [styles.root_isGhost]: ghost,
+        [styles[`root_is${capitalize(color)}`]]: color
       }),
       disabled: loading,
       ref: this.storeButtonReference,
