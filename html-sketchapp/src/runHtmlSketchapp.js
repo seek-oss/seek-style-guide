3e7282db41d12a91f8f1cba59699d841441497dd
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2017-12-01 15:23:48 +1100

    WIP Use native finally clause

--- html-sketchapp/src/runHtmlSketchapp.js
+++ html-sketchapp/src/runHtmlSketchapp.js
@@ -1,10 +1,10 @@
-const bluebird = require('bluebird');
+const { promisify, promisifyAll } = require('bluebird');
 const getPort = require('get-port');
 const serve = require('serve');
 const puppeteer = require('puppeteer');
-const waitOn = bluebird.promisify(require('wait-on'));
-const mkdirp = bluebird.promisify(require('mkdirp'));
-const fs = bluebird.promisifyAll(require('fs'));
+const waitOn = promisify(require('wait-on'));
+const mkdirp = promisify(require('mkdirp'));
+const fs = promisifyAll(require('fs'));
 const path = require('path');
 
 // Config:
@@ -19,60 +19,56 @@ const symbolViewports = [
   { name: 'Mobile', width: 320, height: 568 }
 ];
 
-let server;
-
-const runHtmlSketchapp = bluebird.method(async() => {
+(async function() {
   const port = await getPort();
-  const symbolsUrl = `http://localhost:${String(port)}/sketch-exports`;
-
   const docsPath = path.resolve(__dirname, '../../docs/dist');
-  server = serve(docsPath, { port, silent: true });
-  await waitOn({ resources: [symbolsUrl] });
+  const server = serve(docsPath, { port, silent: true });
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  try {
+    const symbolsUrl = `http://localhost:${String(port)}/sketch-exports`;
+    await waitOn({ resources: [symbolsUrl] });
 
-  await page.goto(symbolsUrl, { waitUntil: 'networkidle0' });
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  const bundlePath = path.resolve(__dirname, '../dist/convertToAlmostSketch.bundle.js');
-  const bundle = await fs.readFileAsync(bundlePath, 'utf8');
-  await page.addScriptTag({ content: bundle });
+    await page.goto(symbolsUrl, { waitUntil: 'networkidle0' });
 
-  await page.evaluate('convertToAlmostSketch.setupStyles()');
-  await page.evaluate(`convertToAlmostSketch.setupSymbols({ name: ${JSON.stringify(`${scope} Symbols`)} })`);
+    const bundlePath = path.resolve(__dirname, '../dist/convertToAlmostSketch.bundle.js');
+    const bundle = await fs.readFileAsync(bundlePath, 'utf8');
+    await page.addScriptTag({ content: bundle });
 
-  await page.evaluate('convertToAlmostSketch.snapshotColorStyles()');
+    await page.evaluate('convertToAlmostSketch.setupStyles()');
+    await page.evaluate(`convertToAlmostSketch.setupSymbols({ name: ${JSON.stringify(`${scope} Symbols`)} })`);
 
-  for (const textViewport of textViewports) {
-    await page.setViewport({ width: textViewport.width, height: textViewport.height });
-    await page.evaluate(`convertToAlmostSketch.snapshotTextStyles({ suffix: "/${textViewport.name}" })`);
-  }
+    await page.evaluate('convertToAlmostSketch.snapshotColorStyles()');
 
-  for (const symbolViewport of symbolViewports) {
-    await page.setViewport({ width: symbolViewport.width, height: symbolViewport.height });
-    await page.evaluate(`convertToAlmostSketch.snapshotSymbols({ suffix: "/${symbolViewport.name}" })`);
-  }
+    for (const textViewport of textViewports) {
+      await page.setViewport({ width: textViewport.width, height: textViewport.height });
+      await page.evaluate(`convertToAlmostSketch.snapshotTextStyles({ suffix: "/${textViewport.name}" })`);
+    }
 
-  const asketchStylesJSON = await page.evaluate('convertToAlmostSketch.getStylesJSON()');
-  const asketchSymbolsJSON = await page.evaluate('convertToAlmostSketch.getSymbolsJSON()');
+    for (const symbolViewport of symbolViewports) {
+      await page.setViewport({ width: symbolViewport.width, height: symbolViewport.height });
+      await page.evaluate(`convertToAlmostSketch.snapshotSymbols({ suffix: "/${symbolViewport.name}" })`);
+    }
 
-  const outputPath = path.join(__dirname, '../../dist/asketch');
-  const outputSymbolsPath = path.join(outputPath, 'symbols.asketch.json');
-  const outputStylesPath = path.join(outputPath, 'styles.asketch.json');
+    const asketchStylesJSON = await page.evaluate('convertToAlmostSketch.getStylesJSON()');
+    const asketchSymbolsJSON = await page.evaluate('convertToAlmostSketch.getSymbolsJSON()');
 
-  await mkdirp(outputPath);
-  await Promise.all([
-    fs.writeFileAsync(outputSymbolsPath, asketchSymbolsJSON),
-    fs.writeFileAsync(outputStylesPath, asketchStylesJSON)
-  ]);
+    const outputPath = path.join(__dirname, '../../dist/asketch');
+    const outputSymbolsPath = path.join(outputPath, 'symbols.asketch.json');
+    const outputStylesPath = path.join(outputPath, 'styles.asketch.json');
 
-  console.log('💎 Successfully extracted Sketch symbols and document styles.');
+    await mkdirp(outputPath);
+    await Promise.all([
+      fs.writeFileAsync(outputSymbolsPath, asketchSymbolsJSON),
+      fs.writeFileAsync(outputStylesPath, asketchStylesJSON)
+    ]);
 
-  browser.close();
-});
+    console.log('💎 Successfully extracted Sketch symbols and document styles.');
 
-runHtmlSketchapp().finally(() => {
-  if (server) {
+    browser.close();
+  } finally {
     server.stop();
   }
-});
+}());
