280a7a5f7ecb8860f3e2c7f250524972333b9ff7
Kosanna <kosanna.vipmail@gmail.com>
2017-12-06 15:32:53 +1100

    feat(Rating): Adds sizes and optional text rating to stars
    Add size prop which could be one of superstandard, substandard, heading which scales icons
    respectively. Add prop showTextRating which shows inline rating in text and scales down the icons

--- react/Rating/Rating.test.js
+++ react/Rating/Rating.test.js
@@ -14,11 +14,29 @@ describe('Rating', () => {
   });
 
   describe('should render correct star rating', () => {
-    const ratings = ['2.74', '2.75', '3.74', '3.75', '0', '5'];
+    const ratings = [2.74, 2.75, 3.74, 3.75, 0.0, 5.0];
 
     ratings.forEach(rating => {
       it(`when rating is: ${rating}`, () => {
         const wrapper = shallow(<Rating rating={rating} />);
+        expect(wrapper).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('should render correct size', () => {
+    const sizes = ['substandard', 'standard', 'heading'];
+
+    sizes.forEach(size => {
+      it(`when size is: ${size}`, () => {
+        const wrapper = shallow(<Rating rating={3.5} size={size} />);
+        expect(wrapper).toMatchSnapshot();
+      });
+    });
+
+    sizes.forEach(size => {
+      it(`when size is: ${size} and rating text is shown`, () => {
+        const wrapper = shallow(<Rating rating={4} size={size} showTextRating />);
         expect(wrapper).toMatchSnapshot();
       });
     });
