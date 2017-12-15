a4ee3758df77996fc0ab264b4b840311cfc4a522
Kosanna <kosanna.vipmail@gmail.com>
2017-12-15 11:50:56 +1100

    Add all sizes tests

--- react/Text/Text.test.js
+++ react/Text/Text.test.js
@@ -2,13 +2,19 @@ import { shallow } from 'enzyme';
 import React from 'react';
 import Text from './Text';
 
+const sizes = ['small', 'substandard', 'superstandard', 'subheading', 'headline', 'heading', 'hero'];
+
 describe('Text', () => {
   it('should render with defaults', () => {
     expect(shallow(<Text>Hello</Text>)).toMatchSnapshot();
   });
 
-  it('should render as heading', () => {
-    expect(shallow(<Text heading>Hello</Text>)).toMatchSnapshot();
-    expect(shallow(<Text size="heading">Hello</Text>)).toMatchSnapshot();
+  describe('sizes', () => {
+    sizes.forEach(size => {
+      it(`should render as ${size}`, () => {
+        expect(shallow(<Text {...{[size]: true}}>Hello</Text>)).toMatchSnapshot();
+        expect(shallow(<Text size={size}>Hello</Text>)).toMatchSnapshot();
+      });
+    });
   });
 });
