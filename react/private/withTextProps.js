f97b5d112c9a4b979824b2343a539458beb315a2
Kosanna <kosanna.vipmail@gmail.com>
2017-12-15 11:46:27 +1100

    Add size prop getter

--- react/private/withTextProps.js
+++ react/private/withTextProps.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import omit from 'lodash/omit';
 
+const sizes = ['small', 'substandard', 'superstandard', 'subheading', 'headline', 'heading', 'hero'];
+const getSizeProps = props => {
+  const sizeProps = {};
+
+  sizes.forEach(size => {
+    sizeProps[size] = props.size === size || props[size];
+    return;
+  });
+
+  return sizeProps;
+};
+
 const withTextProps = () => WrappedComponent => {
   return props => {
     const newProps = {
       ...omit(props, 'size'),
-      heading: props.size === 'heading' || props.heading
+      ...getSizeProps(props)
     };
 
     return <WrappedComponent {...newProps} />;
