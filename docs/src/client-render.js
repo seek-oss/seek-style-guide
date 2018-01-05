3c264760a7b19e610d057bda4340c1a4017c27ee
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2018-01-05 11:43:48 +1100

    docs(site): shim Array.includes for IE11

--- docs/src/client-render.js
+++ docs/src/client-render.js
@@ -3,6 +3,11 @@ import { hydrate, render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import BrowserRouter from './BrowserRouter';
 
+import includes from 'array-includes';
+if (!Array.prototype.includes) {
+  includes.shim();
+}
+
 const appElement = document.getElementById('app');
 
 hydrate(
