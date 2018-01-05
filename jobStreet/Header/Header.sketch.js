35e8429b453c344445a688db3aa669757532998e
Jye Nicolson <jnicolson@seek.com.au>
2018-01-05 18:07:36 +1100

    feat(sketchExport): merge seek-style-guide sketch support

--- jobStreet/Header/Header.sketch.js
+++ jobStreet/Header/Header.sketch.js
@@ -2,6 +2,5 @@ import React from 'react';
 import Header from './Header';
 
 export const blockSymbols = {
-  'JobStreet/Header/Logged out': <Header user={{}} />,
-  'JobStreet/Header/Logged in': <Header user={{ candidate: { id: 1, username: 'Rose' } }} />
+  'JobStreet/Header/Malaysia EN': <Header country="my" language="en" />
 };
