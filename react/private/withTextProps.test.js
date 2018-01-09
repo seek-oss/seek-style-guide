2da737cdd343e358c59d82e29db3a5ecea704fa3
Kosanna <kosanna.vipmail@gmail.com>
2018-01-09 13:32:08 +1100

    Add test for unknown string size prop

--- react/private/withTextProps.test.js
+++ react/private/withTextProps.test.js
@@ -21,7 +21,14 @@ describe('withTextProps:', () => {
     expect(shallow(<DecoratedComponent className='foo' bar baz={{ a: 1 }} />)).toMatchSnapshot();
   });
 
-  it('should omit size prop if boolean size is supplied', () => {
+  it('should throw prop validation error if size is unknown', () => {
+    shallow(<DecoratedComponent size="head" />);
+
+    const expectedError = expect.stringMatching(/Invalid prop size='head' supplied to OriginalComponent/);
+    expect(spy).toBeCalledWith(expectedError);
+  });
+
+  it('should omit size prop if boolean size is supplied and throw prop validation error', () => {
     expect(shallow(<DecoratedComponent size="heading" hero />)).toMatchSnapshot();
 
     const expectedError = expect.stringMatching(/Seems that you've accidentially supplied boolean size along with size='heading' to OriginalComponent, please remove one of them. Otherwise boolean prop will overwrite the 'size' prop/);
