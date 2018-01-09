b75f3ed816bfe35e4c8104fb51f564a2cd546cf8
Kosanna <kosanna.vipmail@gmail.com>
2018-01-09 13:27:57 +1100

    Add prop validation test

--- react/private/withTextProps.test.js
+++ react/private/withTextProps.test.js
@@ -7,12 +7,25 @@ const OriginalComponent = props => <div {...props} />;
 const DecoratedComponent = withTextProps(OriginalComponent);
 
 describe('withTextProps:', () => {
+  let spy;
+
+  beforeEach(() => {
+    spy = jest.spyOn(global.console, 'error');
+  });
+
+  afterEach(() => {
+    spy.mockRestore();
+  });
+
   it('should pass down non-size props', () => {
     expect(shallow(<DecoratedComponent className='foo' bar baz={{ a: 1 }} />)).toMatchSnapshot();
   });
 
   it('should omit size prop if boolean size is supplied', () => {
     expect(shallow(<DecoratedComponent size="heading" hero />)).toMatchSnapshot();
+
+    const expectedError = expect.stringMatching(/Seems that you've accidentially supplied boolean size along with size='heading' to OriginalComponent, please remove one of them. Otherwise boolean prop will overwrite the 'size' prop/);
+    expect(spy).toBeCalledWith(expectedError);
   });
 
   sizes.forEach(size => {
