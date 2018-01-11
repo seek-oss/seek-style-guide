f90390735d5c66a355befb79bb4667a03c3e09dc
George Spyropoulos <gspyropoulos@seek.com.au>
2018-01-11 17:27:17 +1100

    fix(Alert): Snapshot tests for the section component

---
+++ react/Section/Section.test.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Section, { TYPE, LEVEL } from './Section';
+
+const renderSection = props => shallow(
+  <Section {...props} >
+    Test content
+  </Section>
+);
+
+describe('Section:', () => {
+  describe('types:', () => {
+    it('should render positive sections', () => {
+      const section = renderSection({ type: TYPE.POSITIVE });
+      expect(section).toMatchSnapshot();
+    });
+
+    it('should render info sections', () => {
+      const section = renderSection({ type: TYPE.INFO });
+      expect(section).toMatchSnapshot();
+    });
+
+    it('should render critical sections', () => {
+      const section = renderSection({ type: TYPE.CRITICAL });
+      expect(section).toMatchSnapshot();
+    });
+
+    it('should render help sections', () => {
+      const section = renderSection({ type: TYPE.HELP });
+      expect(section).toMatchSnapshot();
+    });
+  });
+
+  describe('levels:', () => {
+    it('should render primary sections', () => {
+      const section = renderSection({ level: LEVEL.PRIMARY });
+      expect(section).toMatchSnapshot();
+    });
+
+    it('should render secondary sections', () => {
+      const section = renderSection({ level: LEVEL.SECONDARY });
+      expect(section).toMatchSnapshot();
+    });
+  });
+
+  it('should render the pullout style', () => {
+    const section = renderSection({ pullout: true });
+    expect(section).toMatchSnapshot();
+  });
+
+  it('should render the slim style', () => {
+    const section = renderSection({ slim: true });
+    expect(section).toMatchSnapshot();
+  });
+
+  it('should render the header style', () => {
+    const section = renderSection({ header: true });
+    expect(section).toMatchSnapshot();
+  });
+
+  it('should accept additional classnames and place them on the root', () => {
+    const section = renderSection({ level: LEVEL.SECONDARY, className: 'testClass' });
+    expect(section).toMatchSnapshot();
+  });
+
+  it('should accept additional props', () => {
+    const section = renderSection({ testProp: true });
+    expect(section).toMatchSnapshot();
+  });
+
+  it('should render multiple classes together', () => {
+    const section = renderSection({ level: LEVEL.SECONDARY, type: TYPE.INFO, slim: true });
+    expect(section).toMatchSnapshot();
+  });
+});
