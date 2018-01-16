4a02b645be95b7f9ad454966b889694838c9021f
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2018-01-16 11:20:05 +1100

    chore(sketch): Use Less variables to generate Text symbol names

--- react/Text/Text.sketch.js
+++ react/Text/Text.sketch.js
@@ -1,28 +1,54 @@
 import React from 'react';
 import mapKeys from 'lodash/mapKeys';
+import lessToJs from 'less-vars-to-js';
+import type from '!!raw-loader!seek-style-guide/theme/type/type.less';
 import Text from './Text';
 
+const sizes = [
+  'hero',
+  'headline',
+  'heading',
+  'subheading',
+  'superstandard',
+  'standard',
+  'substandard',
+  'small'
+];
+const typeVars = lessToJs(type);
+const baseFontSize = parseInt(typeVars['@base-font-size'], 10);
+const symbolNames = sizes.reduce((obj, size, i) => {
+  const [ firstChar, ...restChars ] = size;
+  const name = `${firstChar.toUpperCase()}${restChars.join('')}`;
+
+  const desktopSize = parseFloat(typeVars[`@${size}-type-scale`]) * baseFontSize;
+  const mobileSize = parseFloat(typeVars[`@${size}-type-scale-mobile`]) * baseFontSize;
+
+  const symbolName = `${i + 1}. ${name} — Desktop: ${desktopSize}px, Mobile: ${mobileSize}px`;
+
+  return { ...obj, [size]: symbolName };
+}, {});
+
 export const text = {
-  '1. Hero — Desktop: 42px, Mobile: 21px': <Text hero>Hero text</Text>,
-  '2. Headline — Desktop: 28px, Mobile: 21px': <Text headline>Headline text</Text>,
-  '3. Heading — Desktop: 21px, Mobile: 21px': <Text heading>Heading text</Text>,
-  '4. Subheading — Desktop: 18px, Mobile: 21px': <Text subheading>Subheading text</Text>,
-  '5. Superstandard — Desktop: 18px, Mobile: 18px/1. Default': <Text superstandard>Superstandard text</Text>,
-  '5. Superstandard — Desktop: 18px, Mobile: 18px/2. Secondary': <Text superstandard secondary>Superstandard critical text</Text>,
-  '5. Superstandard — Desktop: 18px, Mobile: 18px/3. Strong': <Text superstandard strong>Superstandard strong text</Text>,
-  '5. Superstandard — Desktop: 18px, Mobile: 18px/4. Positive': <Text superstandard positive>Superstandard positive text</Text>,
-  '5. Superstandard — Desktop: 18px, Mobile: 18px/5. Critical': <Text superstandard critical>Superstandard critical text</Text>,
-  '6. Standard — Desktop: 14px, Mobile: 18px/1. Default': <Text>Standard text</Text>,
-  '6. Standard — Desktop: 14px, Mobile: 18px/2. Secondary': <Text secondary>Standard critical text</Text>,
-  '6. Standard — Desktop: 14px, Mobile: 18px/3. Strong': <Text strong>Standard strong text</Text>,
-  '6. Standard — Desktop: 14px, Mobile: 18px/4. Positive': <Text positive>Standard positive text</Text>,
-  '6. Standard — Desktop: 14px, Mobile: 18px/5. Critical': <Text critical>Standard critical text</Text>,
-  '7. Substandard — Desktop: 14px, Mobile: 14px/1. Default': <Text substandard>Substandard text</Text>,
-  '7. Substandard — Desktop: 14px, Mobile: 14px/2. Secondary': <Text substandard secondary>Substandard critical text</Text>,
-  '7. Substandard — Desktop: 14px, Mobile: 14px/3. Strong': <Text substandard strong>Substandard strong text</Text>,
-  '7. Substandard — Desktop: 14px, Mobile: 14px/4. Positive': <Text substandard positive>Substandard positive text</Text>,
-  '7. Substandard — Desktop: 14px, Mobile: 14px/5. Critical': <Text substandard critical>Substandard critical text</Text>,
-  '8. Small — Desktop: 12px, Mobile: 12px': <Text small>Small text</Text>
+  [`${symbolNames.hero}`]: <Text hero>Hero text</Text>,
+  [`${symbolNames.headline}`]: <Text headline>Headline text</Text>,
+  [`${symbolNames.heading}`]: <Text heading>Heading text</Text>,
+  [`${symbolNames.subheading}`]: <Text subheading>Subheading text</Text>,
+  [`${symbolNames.superstandard}/1. Default`]: <Text superstandard>Superstandard text</Text>,
+  [`${symbolNames.superstandard}/2. Secondary`]: <Text superstandard secondary>Superstandard critical text</Text>,
+  [`${symbolNames.superstandard}/3. Strong`]: <Text superstandard strong>Superstandard strong text</Text>,
+  [`${symbolNames.superstandard}/4. Positive`]: <Text superstandard positive>Superstandard positive text</Text>,
+  [`${symbolNames.superstandard}/5. Critical`]: <Text superstandard critical>Superstandard critical text</Text>,
+  [`${symbolNames.standard}/1. Default`]: <Text>Standard text</Text>,
+  [`${symbolNames.standard}/2. Secondary`]: <Text secondary>Standard critical text</Text>,
+  [`${symbolNames.standard}/3. Strong`]: <Text strong>Standard strong text</Text>,
+  [`${symbolNames.standard}/4. Positive`]: <Text positive>Standard positive text</Text>,
+  [`${symbolNames.standard}/5. Critical`]: <Text critical>Standard critical text</Text>,
+  [`${symbolNames.substandard}/1. Default`]: <Text substandard>Substandard text</Text>,
+  [`${symbolNames.substandard}/2. Secondary`]: <Text substandard secondary>Substandard critical text</Text>,
+  [`${symbolNames.substandard}/3. Strong`]: <Text substandard strong>Substandard strong text</Text>,
+  [`${symbolNames.substandard}/4. Positive`]: <Text substandard positive>Substandard positive text</Text>,
+  [`${symbolNames.substandard}/5. Critical`]: <Text substandard critical>Substandard critical text</Text>,
+  [`${symbolNames.small}`]: <Text small>Small text</Text>
 };
 
 // Export text styles as symbols
