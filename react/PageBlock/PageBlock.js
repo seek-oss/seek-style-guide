529f2536d9ea2de18a47c15be03f3ab43fbb1997
Jahred Hope <jahredhope@gmail.com>
2018-01-17 15:00:08 +1100

    feat(PageBlock): Add support for wide attribute

--- react/PageBlock/PageBlock.js
+++ react/PageBlock/PageBlock.js
@@ -4,17 +4,16 @@ import PropTypes from 'prop-types';
 
 import classnames from 'classnames';
 
-export default function PageBlock({ children, className, ...restProps }) {
+export default function PageBlock({ children, className, wide, ...restProps }) {
   return (
     <div {...restProps} className={classnames(className)}>
-      <div className={styles.content}>
-        {children}
-      </div>
+      <div className={classnames({ [styles.content]: true, [styles.wide]: wide })}>{children}</div>
     </div>
   );
 }
 
 PageBlock.propTypes = {
   className: PropTypes.string,
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  wide: PropTypes.bool
 };
