1778869f4847c640e72604b49027c46b8b9b3e77
George <georgesinbox@gmail.com>
2018-01-31 09:52:08 +1100

    feat(Alert): Add Alert component (#415)
    * feat(Alert): New alert component
    
    The alert component can be used to display timely information to users
    
    * fix(Alert): First cut of styling for alerts
    
    * fix(Alert): Add ciritical icon to the styleguide
    
    * fix(Alert): More alert styling
    
    * fix(Alert): Styling tweaks
    
    * fix(PackageJson): Remove lock file
    
    * fix(Alert): Add a breakout option in the section component
    
    * fix(Alert): Remove the empty test for alerts to allow the build to complete
    
    Will add this back at a later stage
    
    * fix(Alert): Use section to provide the background colour to alerts
    
    * fix(Section): Add a demo page for Section
    
    * fix(Alerts): Tidy up the Section demo and provide better default props
    
    * fix(Alert): Tidy up demo and fix styling bug
    
    * fix(Alert): Tidy up
    
    * fix(Alerts): Snapshot tests for the alert component
    
    * fix(Alert): Snapshot tests for the section component
    
    * fix(alert): Pr feedback

---
+++ react/Alert/Alert.test.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Alert from './Alert';
+import { TONE, LEVEL } from '../Section/Section';
+
+const renderAlert = props => shallow(
+  <Alert
+    tone={TONE.POSITIVE}
+    level={LEVEL.PRIMARY}
+    message="Test message"
+    {...props}
+  />
+);
+
+describe('Alert:', () => {
+  describe('types:', () => {
+    it('should render positive alerts', () => {
+      const alert = renderAlert({ tone: TONE.POSITIVE });
+      expect(alert).toMatchSnapshot();
+    });
+
+    it('should render info alerts', () => {
+      const alert = renderAlert({ tone: TONE.INFO });
+      expect(alert).toMatchSnapshot();
+    });
+
+    it('should render critical alerts', () => {
+      const alert = renderAlert({ tone: TONE.CRITICAL });
+      expect(alert).toMatchSnapshot();
+    });
+
+    it('should render help alerts', () => {
+      const alert = renderAlert({ tone: TONE.HELP });
+      expect(alert).toMatchSnapshot();
+    });
+  });
+
+  describe('levels:', () => {
+    it('should render primary alerts', () => {
+      const alert = renderAlert({ level: LEVEL.PRIMARY });
+      expect(alert).toMatchSnapshot();
+    });
+
+    it('should render secondary alerts', () => {
+      const alert = renderAlert({ level: LEVEL.SECONDARY });
+      expect(alert).toMatchSnapshot();
+    });
+
+    it('should render tertiary alerts', () => {
+      const alert = renderAlert({ level: LEVEL.TERTIARY });
+      expect(alert).toMatchSnapshot();
+    });
+  });
+
+  it('should render the pullout style', () => {
+    const alert = renderAlert({ pullout: true });
+    expect(alert).toMatchSnapshot();
+  });
+
+  it('should render with a hidden icon', () => {
+    const alert = renderAlert({ hideIcon: true });
+    expect(alert).toMatchSnapshot();
+  });
+
+  it('should render a close button', () => {
+    const alert = renderAlert({ onClose: true });
+    expect(alert).toMatchSnapshot();
+  });
+
+  it('should call a passed in onClose function when the button fires a click event', () => {
+    const onClose = jest.fn();
+    const alert = renderAlert({ onClose });
+    const event = { someEventData: true };
+
+    alert.find('button').simulate('click', event);
+    expect(onClose).toBeCalledWith(event);
+  });
+});
