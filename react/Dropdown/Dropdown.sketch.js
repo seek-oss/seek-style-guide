d7e3d82bd6bf1529feab6300af15616a2c5fac54
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2018-02-07 10:05:25 +1100

    chore(sketch): Add symbols for Dropdown, Textarea, Text links (#425)

---
+++ react/Dropdown/Dropdown.sketch.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Dropdown from './Dropdown';
+import styles from './Dropdown.sketch.less';
+import SketchFieldContainer from '../private/SketchFieldContainer/SketchFieldContainer';
+
+const commonProps = {
+  className: styles.root,
+  label: 'Label',
+  placeholder: 'Placeholder text',
+  options: [],
+  inputProps: {
+    value: '',
+    onChange: () => {}
+  }
+};
+
+export const symbols = {
+  'Dropdown/Standard': (
+    <SketchFieldContainer>
+      <Dropdown {...commonProps} id="dropdown1" message={false} />
+    </SketchFieldContainer>
+  ),
+  'Dropdown/With Help Text': (
+    <SketchFieldContainer>
+      <Dropdown {...commonProps} id="dropdown2" message="Help text" />
+    </SketchFieldContainer>
+  ),
+  'Dropdown/With Critical Text': (
+    <SketchFieldContainer>
+      <Dropdown {...commonProps} id="dropdown3" message="Critical text" valid={false} />
+    </SketchFieldContainer>
+  ),
+  'Dropdown/With Positive Text': (
+    <SketchFieldContainer>
+      <Dropdown {...commonProps} id="dropdown4" message="Positive text" valid={true} />
+    </SketchFieldContainer>
+  )
+};
