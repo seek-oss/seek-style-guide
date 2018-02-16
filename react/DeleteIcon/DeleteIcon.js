503c3cd439adedf8e1e5ffbce568fcf8e3998d95
Tom Gillard <tomgillar@gmail.com>
2018-02-16 13:42:52 +1100

    fix(icons): Updated delete and plus icon components to use private icon component

--- react/DeleteIcon/DeleteIcon.js
+++ react/DeleteIcon/DeleteIcon.js
@@ -1,31 +1,10 @@
 import svgMarkup from './DeleteIcon.svg';
 
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import Icon from '../private/Icon/Icon';
 
-export default class DeleteIcon extends Component {
-  static displayName = 'DeleteIcon';
-
-  static propTypes = {
-    svgClassName: PropTypes.string
-  };
-
-  static defaultProps = {
-    svgClassName: ''
-  };
-
-  render() {
-    const { svgClassName, ...restProps } = this.props;
-    const svgMarkupWithClassName = svgMarkup
-      .replace('<svg ', `<svg class="${svgClassName}" `);
-
-    /* eslint-disable react/no-danger */
-    return (
-      <span
-        dangerouslySetInnerHTML={{ __html: svgMarkupWithClassName }}
-        {...restProps}
-      />
-    );
-    /* eslint-enable react/no-danger */
-  }
+export default function DeleteIcon(props) {
+  return <Icon markup={svgMarkup} {...props} />;
 }
+
+DeleteIcon.displayName = 'DeleteIcon';
