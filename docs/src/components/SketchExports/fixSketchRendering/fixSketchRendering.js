43357d75d7f9748d182d84079205be2411bec0d5
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2018-03-28 08:27:04 +1100

    refactor: Update comment

--- docs/src/components/SketchExports/fixSketchRendering/fixSketchRendering.js
+++ docs/src/components/SketchExports/fixSketchRendering/fixSketchRendering.js
@@ -32,7 +32,7 @@ export default rootEl => {
 
   // Another hack to remove visually-hidden elements that html-sketchapp erroneously renders
   Array.from(rootEl.querySelectorAll('*')).forEach(el => {
-    // Don't remove checkboxes or it breaks our custom checkbox CSS rules
+    // Don't remove checkboxes and radio buttons or it breaks our CSS
     // Plus, Sketch doesn't seem to render them, anyway
     if (el.nodeName === 'INPUT' && /checkbox|radio/.test(el.type)) {
       return;
