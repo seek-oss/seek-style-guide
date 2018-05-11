b4aa2ddc9b60276dbd23c4502faab48dd24af499
Patrick Liu <patrickliu@seekasia.com>
2018-05-11 17:13:17 +0800

    fix(reactDBfooter): Revoke to CFS version layout design

--- jobsDB/Footer/Footer.js
+++ jobsDB/Footer/Footer.js
@@ -2,104 +2,97 @@ import React, { Component } from 'react';
 import styles from './footer.less';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-//import FooterLinks from './components/FooterLinks/FooterLinks';
+// import FooterLinks from './components/FooterLinks/FooterLinks';
 import localization from './localization';
 
-const Footer = ({ language, country, baseUrl, mbaseUrl, contentUrl}) => {
-  
-const year = new Date().getFullYear();
-const messages = localization[`${language}-${country}`];
+const Footer = ({ language, country, baseUrl, mbaseUrl, contentUrl }) => {
+  const year = new Date().getFullYear();
+  const messages = localization[`${language}-${country}`];
+  const footerbackground = `${contentUrl}/images/Shared/v8Revamp2/logo_ftr_blue.png`;
+  const footerbackgroundimage = `${contentUrl}/images/Shared/v8Revamp2/logo_ftr_blue.svg`;
+  const socialbackgroundimage = `${contentUrl}/images/Shared/v8Revamp2/icon_footer-social-all.svg`;
+  const backgroudstyle = {
+    background: `url(${footerbackground}), `,
+    backgroundImage: `url(${footerbackgroundimage}), none`
+  };
 
-const footerbackground = contentUrl+"/images/Shared/v8Revamp2/logo_ftr_blue.png";
-const footerbackgroundimage = contentUrl+"/images/Shared/v8Revamp2/logo_ftr_blue.svg";
-const socialbackgroundimage = contentUrl+"/images/Shared/v8Revamp2/icon_footer-social-all.svg";
- 
-const backgroudstyle = {
-  background: `url(${footerbackground}), `,
-  backgroundImage: `url(${footerbackgroundimage}), none`
-};
+  const socialbackgroundimagestyle = {
+    backgroundImage: `url(${socialbackgroundimage})`
+  };
 
-const socialbackgroundimagestyle = {
-  backgroundImage: `url(${socialbackgroundimage})`
-};
-
-return (
+  return (
     <footer className={styles['is-full']}>
       <div className={styles['footer--full']}>
         <div className={styles.container}>
           <div className={styles.row}>
             <div className={styles['col-md-4']}>
               <div className={styles.row}>
-                <div className={classnames(styles['col-sm-6'],styles['col-md-12'])}>
-                  <h2 className={classnames(styles['footer-logo'],styles['hidden-xs'])}><a style={backgroudstyle} href="#">jobsDB.com</a></h2>
+                <div className={classnames(styles['col-sm-6'], styles['col-md-12'])}>
+                  <h2 className={classnames(styles['footer-logo'], styles['hidden-xs'])}><a style={backgroudstyle} href="#">jobsDB.com</a></h2>
                 </div>
-                <div className={classnames(styles['col-xs-12'],styles['col-sm-6'],styles['col-md-12'])}>
-                  <div className={styles.row}>
-                    <div className={classnames(styles['col-xs-6'],styles['col-xs-push-6'],styles['col-sm-12'],styles['col-sm-push-0'],styles['footer-contact-info'])}>
-                      <div>{messages['footer.jobpostingenquiry']}</div>
-                      <div>{messages['footer.cshotline']} <em>{messages['footer.officehour']}</em></div>
-                      <div><a href={messages['footer.csemailLink']}>{messages['footer.csemail']}</a></div>
-                    </div>
-                    <div className={classnames(styles['col-xs-6'],styles['col-xs-pull-6'],styles['col-sm-12'],styles['col-sm-pull-0'])}>
-                      <ul className={classnames(styles['footer-social'],styles['footer-social-icons'])}>
-                        <li><a className={classnames(styles['social-icon'],styles['social-facebook'])} style={socialbackgroundimagestyle} href={messages['footer.facebookLink']} target="_blank">Facebook</a></li>
-                        <li><a className={classnames(styles['social-icon'],styles['social-googleplus'])} style={socialbackgroundimagestyle} href={messages['footer.googleplusLink']} target="_blank">Google+</a></li>
-                        <li><a className={classnames(styles['social-icon'],styles['social-weibo'])} style={socialbackgroundimagestyle} href={messages['footer.weiboLink']} target="_blank">Weibo</a></li>
-                        <li><a className={classnames(styles['social-icon'],styles['social-twitter'])} style={socialbackgroundimagestyle} href={messages['footer.twitterLink']} target="_blank">Twitter</a></li>
-                        <li><a className={classnames(styles['social-icon'],styles['social-feed'])} style={socialbackgroundimagestyle} href={messages['footer.rssLink']} target="_blank">RSS Feed</a></li>
-                      </ul>
-                      <p></p>
-                    </div>
+                <div className={classnames(styles['col-sm-6'], styles['col-md-12'])}>
+                  <div className={styles['footer-contact-info']}>
+                    <div>{messages['footer.jobpostingenquiry']}</div>
+                    <div>{messages['footer.cshotline']} <em>{messages['footer.officehour']}</em></div>
+                    <div><a href={messages['footer.csemailLink']}>{messages['footer.csemail']}</a></div>
+                    <ul className={classnames(styles['footer-social'], styles['footer-social-icons'])}>
+                      <li><a className={classnames(styles['social-icon'], styles['social-facebook'])} style={socialbackgroundimagestyle} href={messages['footer.facebookLink']} target="_blank">Facebook</a></li>
+                      <li><a className={classnames(styles['social-icon'], styles['social-googleplus'])} style={socialbackgroundimagestyle} href={messages['footer.googleplusLink']} target="_blank">Google+</a></li>
+                      <li><a className={classnames(styles['social-icon'], styles['social-weibo'])} style={socialbackgroundimagestyle} href={messages['footer.weiboLink']} target="_blank">Weibo</a></li>
+                      <li><a className={classnames(styles['social-icon'], styles['social-twitter'])} style={socialbackgroundimagestyle} href={messages['footer.twitterLink']} target="_blank">Twitter</a></li>
+                      <li><a className={classnames(styles['social-icon'], styles['social-feed'])} style={socialbackgroundimagestyle} href={messages['footer.rssLink']} target="_blank">RSS Feed</a></li>
+                    </ul>
+                    <p>&nbsp;</p>
                   </div>
                 </div>
               </div>
             </div>
-            <div className={classnames(styles['col-sm-6'],styles['col-md-4'])}>
+            <div className={classnames(styles['col-sm-6'], styles['col-md-4'])}>
               <div className={styles.row}>
                 <div className={styles['col-xs-6']}>
-                  <dl className={classnames(styles['footer-list'],styles['first'])}>
+                  <dl className={classnames(styles['footer-list'], styles.first)}>
                     <dt>{messages['footer.jobseekers']}</dt>
-                    <dd><a href={baseUrl+messages['footer.browsejobsLink']}>{messages['footer.browsejobs']}</a></dd>
-                    <dd><a href={baseUrl+messages['footer.postajobLink']}>{messages['footer.postresumes']}</a></dd>
-                    <dd><a href={baseUrl+messages['footer.jobalertsLink']}>{messages['footer.jobalerts']}</a></dd>
-                    <dd><a href={baseUrl+messages['footer.myjobsdbLink']}>{messages['footer.myjobsdb']}</a></dd>
-                    <dd><a href={baseUrl+messages['footer.careerinsightsLink']}>{messages['footer.careerinsights']}</a></dd>
+                    <dd><a href={baseUrl + messages['footer.browsejobsLink']}>{messages['footer.browsejobs']}</a></dd>
+                    <dd><a href={baseUrl + messages['footer.postajobLink']}>{messages['footer.postresumes']}</a></dd>
+                    <dd><a href={baseUrl + messages['footer.jobalertsLink']}>{messages['footer.jobalerts']}</a></dd>
+                    <dd><a href={baseUrl + messages['footer.myjobsdbLink']}>{messages['footer.myjobsdb']}</a></dd>
+                    <dd><a href={baseUrl + messages['footer.careerinsightsLink']}>{messages['footer.careerinsights']}</a></dd>
                   </dl>
                 </div>
                 <div className={styles['col-xs-6']}>
                   <dl className={styles['footer-list']}>
                     <dt>{messages['footer.employers']}</dt>
-                    <dd><a href={baseUrl+messages['footer.postajobLink']}>{messages['footer.postajob']}</a></dd>
-                    <dd><a href={baseUrl+messages['footer.searchcandidatesLink']}>{messages['footer.searchcandidates']}</a></dd>
-                    <dd><a href={baseUrl+messages['footer.advertisewithusLink']}>{messages['footer.advertisewithus']}</a></dd>
+                    <dd><a href={baseUrl + messages['footer.postajobLink']}>{messages['footer.postajob']}</a></dd>
+                    <dd><a href={baseUrl + messages['footer.searchcandidatesLink']}>{messages['footer.searchcandidates']}</a></dd>
+                    <dd><a href={baseUrl + messages['footer.advertisewithusLink']}>{messages['footer.advertisewithus']}</a></dd>
                   </dl>
                 </div>
               </div>
             </div>
-            <div className={classnames(styles['col-sm-6'],styles['col-md-4'])}>
+            <div className={classnames(styles['col-sm-6'], styles['col-md-4'])}>
               <div className={styles.row}>
                 <div className={styles['col-xs-6']}>
                   <dl className={styles['footer-list']}>
                     <dt>{messages['footer.aboutus']}</dt>
-                    <dd><a href={baseUrl+messages['footer.aboutjobsdbLink']}>{messages['footer.aboutjobsdb']}</a></dd>
-                    <dd><a href={baseUrl+messages['footer.faqLink']}>{messages['footer.faq']}</a></dd>
-                    <dd><a href={baseUrl+messages['footer.careenatjobsdbLink']}>{messages['footer.careenatjobsdb']}</a></dd>
-                    <dd><a href={baseUrl+messages['footer.contactusLink']}>{messages['footer.contactus']}</a></dd>
+                    <dd><a href={baseUrl + messages['footer.aboutjobsdbLink']}>{messages['footer.aboutjobsdb']}</a></dd>
+                    <dd><a href={baseUrl + messages['footer.faqLink']}>{messages['footer.faq']}</a></dd>
+                    <dd><a href={baseUrl + messages['footer.careenatjobsdbLink']}>{messages['footer.careenatjobsdb']}</a></dd>
+                    <dd><a href={baseUrl + messages['footer.contactusLink']}>{messages['footer.contactus']}</a></dd>
                   </dl>
                 </div>
                 <div className={styles['col-xs-6']}>
                   <dl className={styles['footer-list']}>
                     <dt>{messages['footer.tools']}</dt>
-                    <dd><a href={mbaseUrl+messages['footer.mobileversionLink']} target="_blank">{messages['footer.mobileversion']}</a></dd>
-                    <dd><a href={baseUrl+messages['footer.rssLink']}>{messages['footer.rss']}</a></dd>
+                    <dd><a href={mbaseUrl + messages['footer.mobileversionLink']} target="_blank">{messages['footer.mobileversion']}</a></dd>
+                    <dd><a href={baseUrl + messages['footer.rssLink']}>{messages['footer.rss']}</a></dd>
                   </dl>
                 </div>
               </div>
             </div>
           </div>
           <div className={styles.row}>
-            <div className={classnames(styles['col-md-10'],styles['col-md-offset-1'],styles['text-center'])}>
-              <dl className={classnames(styles['footer-list'],styles['footer-list-horizontal'],styles['first'])}>
+            <div className={classnames(styles['col-md-10'], styles['col-md-offset-1'], styles['text-center'])}>
+              <dl className={classnames(styles['footer-list'], styles['footer-list-horizontal'], styles.first)}>
                 <dt>{messages['footer.internationalpartners']}</dt>
                 <dd><a href={messages['footer.babajobindiaLink']}>{messages['footer.babajobindia']}</a></dd>
                 <dd><a href={messages['footer.bdjobsbandladeshLink']}>{messages['footer.bdjobsbandladesh']}</a></dd>
@@ -123,7 +116,7 @@ return (
       <div className={styles.footer}>
         <div className={styles.container}>
           <div className={styles.copyright}>{messages['footer.copyright'].replace('{year}', year)}{messages['footer.rightReserved']}</div>
-          <div className={styles['base-links']}><a href={baseUrl+messages['footer.privacyLink']} target="_blank">{messages['footer.privacyLinkText']}</a><a href={baseUrl+messages['footer.termsLink']} target="_blank">{messages['footer.termsLinkText']}</a><a href={baseUrl+ messages['footer.siteMapLink']}>{messages['footer.siteMapLinkText']}</a></div>
+          <div className={styles['base-links']}><a href={baseUrl + messages['footer.privacyLink']} target="_blank">{messages['footer.privacyLinkText']}</a><a href={baseUrl + messages['footer.termsLink']} target="_blank">{messages['footer.termsLinkText']}</a><a href={baseUrl + messages['footer.siteMapLink']}>{messages['footer.siteMapLinkText']}</a></div>
         </div>
       </div>
     </footer>
@@ -139,9 +132,9 @@ Footer.propTypes = {
 };
 
 Footer.defaultProps = {
-  baseUrl: "https://hk.jobsdb.com",
-  mbaseUrl: "https://m.jobsdb.com",
-  contentUrl: "https://content.jobsdbcdn.com/Content/d0416"
+  baseUrl: 'https://hk.jobsdb.com',
+  mbaseUrl: 'https://m.jobsdb.com',
+  contentUrl: 'https://content.jobsdbcdn.com/Content/d0416'
 };
 
 export default Footer;
