271b9080083371ebbbc13c7b9c59f6a011484699
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2018-06-19 10:00:30 +1000

    docs(site): Remove critical CSS to improve build performance
    Reverts #455

--- docs/src/server-render.js
+++ docs/src/server-render.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router';
-import purifyCss from 'purify-css';
 import App from 'App/App';
 
 const baseHref = process.env.BASE_HREF || '/';
 
 // Static site renderer
-export default ({ path, template, css }) => new Promise(resolve => {
+export default ({ path, template }) => {
   const html = renderToString(
     <StaticRouter location={path} context={{}}>
       <App />
     </StaticRouter>
   );
 
-  const purifyOptions = {
-    minify: true,
-    whitelist: ['*:not*'] // https://github.com/purifycss/purifycss/issues/161
-  };
-
-  purifyCss(`<html><body>${html}</body></html>`, css, purifyOptions, criticalCss => {
-    resolve(template({ html, baseHref, criticalCss }));
-  });
-});
+  return template({ html, baseHref });
+};
