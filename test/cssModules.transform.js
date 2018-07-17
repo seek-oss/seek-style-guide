2c9c426a653d3eb00c9805086e296957a3e225d9
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2018-07-17 15:37:49 +1000

    test(css): Parse CSS class names in unit tests

---
+++ test/cssModules.transform.js
@@ -0,0 +1,21 @@
+// Really naive, regex-based class name extractor
+
+module.exports = {
+  process: src => {
+    const classNames = [];
+
+    const classNameRegEx = /\.([_a-zA-Z0-9]+)(?=[\.:\[\s{])/gm;
+    let match = classNameRegEx.exec(src);
+    while (match !== null) {
+      classNames.push(match[1]);
+      match = classNameRegEx.exec(src);
+    }
+
+    const cssModule = {};
+    classNames.forEach(className => {
+      cssModule[className] = className;
+    });
+
+    return `module.exports = ${JSON.stringify(cssModule)}`;
+  }
+};
