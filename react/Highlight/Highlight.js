023e89a6b5fb5b60ba030adf8a999a842151db9d
George <georgesinbox@gmail.com>
2018-08-24 09:58:00 +1000

    feat(Highlight): Add the concept of tone to the highlight component (#534)
    * feat(Highlight): Add the concept of tone to the highlight component
    
    * fix(Highlight): Design updates
    
    * fix(Highlight): Reduce the number of tone options
    
    Can add more later if there is a use case for them

--- react/Highlight/Highlight.js
+++ react/Highlight/Highlight.js
@@ -3,22 +3,24 @@ import styles from './Highlight.less';
 import React from 'react';
 import type { Node } from 'react';
 import classnames from 'classnames';
+import { TONE } from '../Section/Section';
 
 type Props = {
   children: Node,
   secondary?: boolean,
-  className?: string
+  className?: string,
+  tone: TONE.CRITICAL | 'neutral'
 };
 
 export default function Highlight(props: Props) {
-  const { children, secondary, className, ...restProps } = props;
-
+  const { children, secondary, tone, className, ...restProps } = props;
   return (
     <mark
       {...restProps}
       className={classnames({
         [styles.root]: true,
-        [styles.secondary]: secondary,
+        [styles.secondaryText]: secondary,
+        [styles[tone]]: true,
         className
       })}>
       {children}
