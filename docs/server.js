b068c54b6a55bd6b1ea2e9c00f28198708ea8ba0
Richard Tan <chardos@users.noreply.github.com>
2018-10-17 14:17:54 +1100

    fix(webpack): Finds an open port on npm start (#557)

--- docs/server.js
+++ docs/server.js
@@ -1,19 +1,27 @@
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.dev.config');
+const createConfigWithPort = require('./webpack.dev.config');
 const opn = require('opn');
-const port = 3000;
+const portfinder = require('portfinder');
+portfinder.basePort = 3000;
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  contentBase: __dirname,
-  hot: true,
-  historyApiFallback: true,
-  disableHostCheck: true
-}).listen(port, '0.0.0.0', error => {
-  if (error) {
-    console.log(error); // eslint-disable-line no-console
-  } else {
-    opn(`http://localhost:${port}`);
-  }
-});
+const startServer = (port) => {
+  const config = createConfigWithPort(port);
+
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    contentBase: __dirname,
+    hot: true,
+    historyApiFallback: true,
+    disableHostCheck: true
+  }).listen(port, '0.0.0.0', error => {
+    if (error) {
+      console.log(error); // eslint-disable-line no-console
+    } else {
+      opn(`http://localhost:${port}`);
+    }
+  });
+}
+
+portfinder.getPortPromise()
+  .then(startServer)
