a10f35ecf97a8fd4665ca46b009d27418d8890a9
Devon Borysiewicz <devon.borysiewicz@gmail.com>
2018-11-01 10:56:36 +1100

    fix(Radio): Fix touchable height and refactor

--- react/Radio/Radio.js
+++ react/Radio/Radio.js
@@ -2,6 +2,7 @@ import styles from './Radio.less';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import classnames from 'classnames';
+import Text from '../Text/Text';
 
 function combineClassNames(props = {}, ...classNames) {
   const { className, ...restProps } = props;
@@ -50,39 +51,27 @@ export default class Radio extends Component {
     return <input {...allInputProps} />;
   }
 
-  renderLabel() {
-    const { label, labelProps, id } = this.props;
+  render() {
+    const { label, labelProps, id, className } = this.props;
     const allLabelProps = {
       htmlFor: id,
       ...combineClassNames(labelProps, styles.label)
     };
 
     return (
-      <label {...allLabelProps}>
-        <svg
-          className={styles.svg}
-          viewBox="0 0 200 200"
-          xmlns="http://www.w3.org/2000/svg"
-          focusable="false" >
-          <circle className={classnames(styles.circle, styles.circle_isHover)} cx="100" cy="100" r="100" />
-          <circle className={classnames(styles.circle, styles.circle_isSelected)} cx="100" cy="100" r="100" />
-        </svg>
-        <span className={styles.labelText}>{label}</span>
-      </label>
-    );
-  }
-
-  render() {
-    const { className } = this.props;
-    const rootClassNames = classnames({
-      [styles.root]: true,
-      [className]: className
-    });
-
-    return (
-      <div className={rootClassNames}>
-        {this.renderInput()}
-        {this.renderLabel()}
+      <div className={className}>
+        <label {...allLabelProps}>
+          {this.renderInput()}
+          <svg
+            className={styles.svg}
+            viewBox="0 0 200 200"
+            xmlns="http://www.w3.org/2000/svg"
+            focusable="false" >
+            <circle className={classnames(styles.circle, styles.circle_isHover)} cx="100" cy="100" r="100" />
+            <circle className={classnames(styles.circle, styles.circle_isSelected)} cx="100" cy="100" r="100" />
+          </svg>
+          <Text raw={true} baseline={false} className={styles.labelText}>{label}</Text>
+        </label>
       </div>
     );
   }
