44dadbcde691f215b4959ee61e0973230314cd73
Richard Tan <chardos@gmail.com>
2019-05-27 15:46:44 +1000

    fix(AccordionItem): Add back the height of auto onAnimationEnd

--- react/AccordionItem/utils.js
+++ react/AccordionItem/utils.js
@@ -1,5 +1,9 @@
 import { CLOSED_HEIGHT, DURATION } from './constants';
 
+function onAnimationStart(fn) {
+  requestAnimationFrame(fn, 0);
+}
+
 function onAnimationEnd(fn) {
   return setTimeout(fn, DURATION);
 }
@@ -29,6 +33,10 @@ export function toggleContent({
 
     setTimeoutHandle(
       onAnimationEnd(() => {
+        // After the animation finishes, we change the height to auto, to account
+        // for things that may change height within the content area, such as 
+        // expanding text fields, etc.
+        setCurrentHeight('auto');
         // We toggle overflow on and off to avoid cropping the focus state
         // on form fields
         setCssOverflow('visible');
@@ -38,9 +46,13 @@ export function toggleContent({
     if (onClose) {
       onClose();
     }
-    setCurrentHeight(CLOSED_HEIGHT);
+    setCurrentHeight(`${contentHeight}px`);
     setCssOverflow('hidden');
     setCssOpacity(0);
+
+    onAnimationStart(() => {
+      setCurrentHeight(CLOSED_HEIGHT);
+    })
 
     setTimeoutHandle(
       onAnimationEnd(() => {
