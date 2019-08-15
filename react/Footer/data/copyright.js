a9ac50da9f070151be51f98068942ed0ca3aba70
Liam S <12887659+lgshaw@users.noreply.github.com>
2019-08-15 16:15:51 +1000

    fix(Footer): Remove sitemap from footer (#640)

--- react/Footer/data/copyright.js
+++ react/Footer/data/copyright.js
@@ -16,11 +16,5 @@ export default [
     href: '/safe-job-searching/',
     rel: 'nofollow',
     analytics: 'toolbar:seek+safely'
-  },
-  {
-    name: 'Site Map',
-    href: '/sitemap',
-    analytics: 'toolbar:site+map',
-    secondary: true
   }
 ];
